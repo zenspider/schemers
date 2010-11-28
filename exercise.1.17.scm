68fe02086240494b1516e2da95eceda56b530168
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-27 21:30:04 -0800

    Did 1.17

--- exercise.1.17.scm
+++ exercise.1.17.scm
@@ -9,13 +9,35 @@
 ;; it is assumed that our language can only add, not multiply) is
 ;; analogous to the `expt' procedure:
 
-(define (* a b)
-  (if (= b 0)
-      0
-      (+ a (* a (- b 1)))))
+;; (define (* a b)
+;;   (if (= b 0) 0
+;;       (+ a (* a (- b 1)))))
 
 ;; This algorithm takes a number of steps that is linear in `b'. Now
 ;; suppose we include, together with addition, operations `double',
 ;; which doubles an integer, and `halve', which divides an (even)
 ;; integer by 2. Using these, design a multiplication procedure
 ;; analogous to `fast-expt' that uses a logarithmic number of steps.
+
+(define (double n) (+ n n))
+(define (halve  n) (/ n 2))             ; that makes no sense...
+
+;; (* 11 10)
+;; (double (* 11 5))
+;; (double (+ 11 (* 11 4)))
+;; (double (+ 11 (double (* 11 2))))
+;; (double (+ 11 (double (double 11))))
+
+(define (mult a b)
+  (define (iterate a b)
+    (cond ((= b 0) 0)
+          ((= b 1) a)
+          ((even? b) (iterate (double a) (halve b)))
+          (else      (+ a (iterate a (- b 1))))))
+  (iterate a b))
+
+;; this isn't strictly tail recursive, or something... doesn't feel right
+
+(mult 11 10)                            ; 110
+(mult 65 64)                            ; 4160
+(mult 3 127)                            ; 381

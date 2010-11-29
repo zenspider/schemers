23d70292bb407f375b6245888066b5c3a3c25776
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-28 22:35:07 -0800

    Added the problem descriptions

--- exercise.1.24.scm
+++ exercise.1.24.scm
@@ -2,4 +2,11 @@
 
 ;;; Exercise 1.24:
 
-;; Description
+;; Modify the `timed-prime-test' procedure of *Note Exercise 1-22:: to
+;; use `fast-prime?' (the Fermat method), and test each of the 12
+;; primes you found in that exercise. Since the Fermat test has
+;; [theta](`log' n) growth, how would you expect the time to test
+;; primes near 1,000,000 to compare with the time needed to test
+;; primes near 1000? Do your data bear this out? Can you explain any
+;; discrepancy you find?
+

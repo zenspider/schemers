9985109410d1bfbb81b89992ef79132ef9cd911c
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-30 02:29:37 -0800

    Did 1.32

--- exercise.1.32.scm
+++ exercise.1.32.scm
@@ -22,3 +22,28 @@
 ;;      write one that generates an iterative process. If it generates
 ;;      an iterative process, write one that generates a recursive
 ;;      process.
+
+(define (prod-old f a b n)
+  (if (> a b) 1
+      (* (f a)
+         (prod-old f (n a) b n))))
+
+(define (accumulate f-c f a b n)
+  (if (> a b) 1
+      (f-c (f a)
+         (accumulate f-c f (n a) b n))))
+
+(define (inc n) (+ n 1))
+(define (identity n) n)
+
+(= (* 10 9 8 7 6 5 4 3 2 1)
+   (prod-old identity 1 10 inc))        ; #t
+
+(= (* 10 9 8 7 6 5 4 3 2 1)
+   (accumulate * identity 1 10 inc))    ; #t
+
+(define (prod-new f a b n)
+  (accumulate * f a b n))
+
+(= (* 10 9 8 7 6 5 4 3 2 1)
+   (prod-new identity 1 10 inc))        ; #t

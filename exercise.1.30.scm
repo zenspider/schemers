1c73bdb595048f4c86539818817816af529e3063
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-30 02:13:50 -0800

    Did 1.30

--- exercise.1.30.scm
+++ exercise.1.30.scm
@@ -8,17 +8,14 @@
 ;; expressions in the following definition:
 
 (define (sum f a b n)
-  (if (> a b) 0
-      (+ (f a)
-         (sum f (n a) b n))))
-
-(define (sum f a b n)
   (define (iterate a result)
     (if (> a b) result
         (iterate (n a) (+ result (f a)))))
   (iterate a 0))
 
+(define (inc n) (+ n 1))
 (define (identity n) n)
+
 (sum identity 1 10 inc)
 
 (= (/ (+ (* 10 10) 10) 2) (sum identity 1 10 inc))

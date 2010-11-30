08715a4a65e926e166ab9eeca8c92c400e077d23
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-30 03:29:23 -0800

    Did 1.34

--- exercise.1.34.scm
+++ exercise.1.34.scm
@@ -3,17 +3,26 @@
 ;;; Exercise 1.34:
 
 ;; Suppose we define the procedure
-;; 
-;;      (define (f g)
-;;        (g 2))
-;; 
+
+(define (f g)
+  (g 2))
+
 ;; Then we have
-;; 
-;;      (f square)
-;;      4
-;; 
-;;      (f (lambda (z) (* z (+ z 1))))
-;;      6
-;; 
+
+(define (square n) (* n n))
+(f square)                              ; 4
+
+(f (lambda (z) (* z (+ z 1))))          ; 6
+
 ;; What happens if we (perversely) ask the interpreter to evaluate
 ;; the combination `(f f)'?  Explain.
+
+;;    (f f)
+
+;; You get an error:
+
+;; procedure application: expected procedure, given: 2; arguments were: 2
+
+;; because:
+
+;; (f f) => (f g=f) => (g=f 2) => (f g=2) => (2 2) => boom

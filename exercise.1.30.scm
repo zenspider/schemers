d4022102e1d54956ca9965d719768147206be6a6
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-29 20:42:47 -0800

    Did 1.30

--- exercise.1.30.scm
+++ exercise.1.30.scm
@@ -6,10 +6,19 @@
 ;; procedure can be rewritten so that the sum is performed
 ;; iteratively. Show how to do this by filling in the missing
 ;; expressions in the following definition:
-;; 
-;;      (define (sum term a next b)
-;;        (define (iter a result)
-;;          (if <??>
-;;              <??>
-;;              (iter <??> <??>)))
-;;        (iter <??> <??>))
+
+(define (sum f a b n)
+  (if (> a b) 0
+      (+ (f a)
+         (sum f (n a) b n))))
+
+(define (sum f a b n)
+  (define (iterate a result)
+    (if (> a b) result
+        (iterate (n a) (+ result (f a)))))
+  (iterate a 0))
+
+(define (identity n) n)
+(sum identity 1 10 inc)
+
+(= (/ (+ (* 10 10) 10) 2) (sum identity 1 10 inc))

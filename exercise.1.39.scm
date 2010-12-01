aefd7e28e328555b42c41485bcdd24b952359df0
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-30 17:16:29 -0800

    Did 1.39

--- exercise.1.39.scm
+++ exercise.1.39.scm
@@ -17,3 +17,28 @@
 ;; computes an approximation to the tangent function based on
 ;; Lambert's formula. `K' specifies the number of terms to compute, as
 ;; in *Note Exercise 1-37::.
+
+;; aren't we done yet? wtf
+
+(define (cont-frac n d i)
+  (define (iterate i fraction)
+    (if (< i 1) fraction
+        (iterate (- i 1) 
+                 (/ (n i)
+                    (+ (d i) fraction)))))
+  (iterate i 0))
+
+(define (tan-cf x k)
+  (let ((nx2 (- (* x x 1.0))))
+    (cont-frac
+     (lambda (i) (if (= i 1) x nx2))
+     (lambda (i) (- (* 2 i) 1))
+     k)))
+
+(tan 10)                                ; 0.6483608274590867
+(tan-cf 10 10)                          ; 0.2202426219807905
+(tan-cf 10 15)                          ; 0.6479824862749116
+(tan-cf 10 20)                          ; 0.6483608247520876
+(tan-cf 10 25)                          ; 0.6483608274590856
+
+

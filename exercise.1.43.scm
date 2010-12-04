13254ad3b990d7d26384f700d118229bead25a20
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-04 01:31:13 -0800

    Did 1.43

--- exercise.1.43.scm
+++ exercise.1.43.scm
@@ -19,3 +19,41 @@
 ;; 
 ;; Hint: You may find it convenient to use `compose' from *Note
 ;; Exercise 1-42::.
+
+(define (square n) (* n n))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (identity n) n)
+
+((compose square square) 5)             ; 625
+
+((compose (compose square square) square) 5) ; 390625
+
+((lambda (x) ((compose square square) x)) 5) ; 625
+
+(define (repeated-twice f)
+  (lambda (x) ((compose f f) x)))
+
+((repeated-twice square) 5)             ; 625
+
+(define (repeated f n)
+  (cond ((= n 0) f)
+        ((= n 1) (compose f identity))
+        (else (repeated (compose f f) (- n 2)))))
+
+(define (repeated-i f n)                ; I'm sure I can do this cleaner
+  (define (iterate f- n)
+    (cond ((= n 0) f-)
+          ((= n 1) (compose f- f))
+          (else (iterate (compose f f) (- n 1)))))
+  (iterate f (- n 1)))                  ; HACK: - here seems wrong
+
+((repeated square 1) 5)                 ; 25
+((repeated square 2) 5)                 ; 625
+((repeated square 3) 5)                 ; this is wrong
+
+((repeated-i square 1) 5)               ; 25
+((repeated-i square 2) 5)               ; 625
+((repeated-i square 3) 5)               ; 390625

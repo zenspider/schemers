9c913d26c6a7394d5341dc1c99f44fe3abedaa1c
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-04 17:54:11 -0800

    Did 1.35

--- exercise.1.35.scm
+++ exercise.1.35.scm
@@ -3,7 +3,25 @@
 ;;; Exercise 1.35:
 
 ;; Show that the golden ratio [phi] (section *Note 1-2-2::) is a fixed
-;; point of the transformation x |-> 1 + 1/x, and use this fact to
-;; compute [phi] by means of the `fixed-point' procedure.
+;; point of the transformation x |-> 1 + 1/x,
 
-;; TODO: ? maybe ?
+;; we showed this umpteen times on previous homework. See 1.13.
+
+;; and use this fact to compute [phi] by means of the `fixed-point'
+;; procedure.
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(< (- (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+      (/ (+ 1 (sqrt 5)) 2))
+   tolerance)

96c59330416a6cac0f0e0c989e2cc165ccf23d3e
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-06 21:13:37 -0800

    2.05

--- sicp/ch_2/exercise.2.05.scm
+++ sicp/ch_2/exercise.2.05.scm
@@ -6,3 +6,19 @@
 ;; numbers and arithmetic operations if we represent the pair a and b
 ;; as the integer that is the product 2^a 3^b. Give the corresponding
 ;; definitions of the procedures `cons', `car', and `cdr'.
+
+(define (count-factors b n count)
+  (if (> (remainder n b) 0) count
+      (count-factors b (/ n b) (+ count 1))))
+
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car p)
+  (count-factors 2 p 0))
+
+(define (cdr p)
+  (count-factors 3 p 0))
+
+(car (cons 2 3))
+(cdr (cons 2 3))

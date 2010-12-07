6ffda8480974e3503d9154e04aaa558d810d591b
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-06 20:59:05 -0800

    2.04

--- sicp/ch_2/exercise.2.04.scm
+++ sicp/ch_2/exercise.2.04.scm
@@ -5,14 +5,20 @@
 ;; Here is an alternative procedural representation of pairs. For this
 ;; representation, verify that `(car (cons x y))' yields `x' for any
 ;; objects `x' and `y'.
-;; 
-;;      (define (cons x y)
-;;        (lambda (m) (m x y)))
-;; 
-;;      (define (car z)
-;;        (z (lambda (p q) p)))
-;; 
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(car (cons (cons 'a 'b) 'c))
+
 ;; What is the corresponding definition of `cdr'? (Hint: To verify
 ;; that this works, make use of the substitution model of section
 ;; *Note 1-1-5::.)
 
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(cdr (cons (cons 'a 'b) 'c))

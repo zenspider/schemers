bb6287da8d0484a2184d3ba154d2319df5017801
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-11 21:34:23 -0800

    2.12

--- sicp/ch_2/exercise.2.12.scm
+++ sicp/ch_2/exercise.2.12.scm
@@ -7,4 +7,23 @@
 ;; also define a selector `percent' that produces the percentage
 ;; tolerance for a given interval. The `center' selector is the same
 ;; as the one shown above.
-;; 
+
+(define make-interval cons)
+(define upper-bound cdr)
+(define lower-bound car)
+
+(define (make-center-percent c p)
+  (make-interval (* c (- 1.0 p)) (* c (+ 1.0 p))))
+
+(define r1 (make-interval (* 0.90 6.8) (* 1.10 6.8)))
+(define r2 (make-interval (* 0.95 4.7) (* 1.05 4.7)))
+
+(define r3 (make-center-percent 6.8 0.1))
+(define r4 (make-center-percent 4.7 0.05))
+
+(define (i= i j)
+  (and (= (upper-bound i) (upper-bound j))
+       (= (lower-bound i) (lower-bound j))))
+
+(i= r1 r3)
+(i= r2 r4)

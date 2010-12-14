2a3d4afd1dee8c788ad417ecf833c8eb3a6e64ff
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-14 00:40:52 -0800

    2.27

--- sicp/ch_2/exercise.2.27.scm
+++ sicp/ch_2/exercise.2.27.scm
@@ -1,13 +1,16 @@
 #lang racket
 
+(require "../lib/testes.rkt")
+
 ;; Exercise 2.27.  
 
-;; Modify your reverse procedure of exercise 2.18 to produce a deep-reverse procedure 
-;; that takes a list as argument and returns as its value the list with its elements 
-;; reversed and with all sublists deep-reversed as well. For example,
-;;
-;; (define x (list (list 1 2) (list 3 4)))
-;;
+;; Modify your reverse procedure of exercise 2.18 to produce a
+;; deep-reverse procedure that takes a list as argument and returns as
+;; its value the list with its elements reversed and with all sublists
+;; deep-reversed as well. For example,
+
+(define x (list (list 1 2) (list 3 4)))
+
 ;; x
 ;; ((1 2) (3 4))
 ;;
@@ -17,3 +20,18 @@
 ;; (deep-reverse x)
 ;; ((4 3) (2 1))
 
+(define (reverse l)
+  (define (iterate l r)
+    (if (null? l) r
+        (iterate (cdr l) (cons (car l) r))))
+  (iterate l null))
+
+(define (deep-reverse l)
+  (define (iterate l r)
+    (cond ((null? l) r)
+          ((pair? (car l)) (iterate (cdr l) (cons (deep-reverse (car l)) r)))
+          (else (iterate (cdr l) (cons (car l) r)))))
+  (iterate l null))
+
+(assert-equal '((3 4) (1 2)) (reverse x))
+(assert-equal '((4 3) (2 1)) (deep-reverse x))

cc03a3fdad7175d080e584f9afa2b7c737418ba7
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-14 00:28:00 -0800

    2.26

--- sicp/ch_2/exercise.2.26.scm
+++ sicp/ch_2/exercise.2.26.scm
@@ -1,8 +1,23 @@
 #lang racket
 
+(require "../lib/testes.rkt")
+
 ;; Exercise 2.26.  
 
 ;; Suppose we define x and y to be two lists:
-;;
-;; (define x (list 1 2 3))
-;; (define y (list 4 5 6))
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+;; What result is printed by the interpreter in response to
+;; evaluating each of the following expressions:
+;; 
+;;      (append x y)
+;; 
+;;      (cons x y)
+;; 
+;;      (list x y)
+
+(assert-equal '(1 2 3 4 5 6)     (append x y))
+(assert-equal '((1 2 3) 4 5 6)   (cons x y))
+(assert-equal '((1 2 3) (4 5 6)) (list x y))

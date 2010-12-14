38477ca37dcdbd48852b6eb9a410186f7b94f512
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-13 21:29:15 -0800

    2.20

--- sicp/ch_2/exercise.2.20.scm
+++ sicp/ch_2/exercise.2.20.scm
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "../lib/testes.rkt")
+
 ;;; Exercise 2.20:
 
 ;; The procedures `+', `*', and `list' take arbitrary numbers of
@@ -39,4 +41,16 @@
 ;; 
 ;;      (same-parity 2 3 4 5 6 7)
 ;;      (2 4 6)
-;; 
+
+(define (same-parity n . l)
+  (let ((parity (remainder n 2)))
+    (define (iterate l r)
+      (if (null? l) r
+          (if (= parity (remainder (car l) 2))
+              (cons (car l) (iterate (cdr l) r))
+              (iterate (cdr l) r))))
+    (cons n (iterate l null))))
+
+(assert-equal '(1 3 5 7) (same-parity 1 2 3 4 5 6 7))
+(assert-equal '(2 4 6)   (same-parity 2 3 4 5 6 7))
+(done)

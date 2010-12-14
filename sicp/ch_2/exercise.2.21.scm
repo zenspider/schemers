f82bf1eeda5a2a1bfdcd04e65e89342e3839a6d1
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-13 21:32:02 -0800

    2.21

--- sicp/ch_2/exercise.2.21.scm
+++ sicp/ch_2/exercise.2.21.scm
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "../lib/testes.rkt")
+
 ;;; Exercise 2.21:
 
 ;; The procedure `square-list' takes a list of numbers as argument and
@@ -18,4 +20,16 @@
 ;; 
 ;;      (define (square-list items)
 ;;        (map <??> <??>))
-;; 
+
+(define (square n) (* n n))
+
+(define (square-list1 items)
+  (if (null? items) null
+      (cons (square (car items)) (square-list1 (cdr items)))))
+
+(define (square-list2 items)
+  (map square items))
+
+(assert-equal '(1 4 9 16) (square-list1 '(1 2 3 4)))
+(assert-equal '(1 4 9 16) (square-list2 '(1 2 3 4)))
+(done)

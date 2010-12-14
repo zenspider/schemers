67b0f7c385d0b04c555501b35e2c91277fe43b81
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-14 00:08:46 -0800

    2.24

--- sicp/ch_2/exercise.2.24.scm
+++ sicp/ch_2/exercise.2.24.scm
@@ -1,7 +1,40 @@
 #lang racket
 
-;; Exercise 2.24.  
+(require "../lib/testes.rkt")
 
-;; Suppose we evaluate the expression (list 1 (list 2 (list 3 4))). Give the result 
-;; printed by the interpreter, the corresponding box-and-pointer structure, and the 
-;; interpretation of this as a tree (as in figure 2.6).
+;;; Exercise 2.24.
+
+;; Suppose we evaluate the expression (list 1 (list 2 (list 3 4))).
+;; Give the result printed by the interpreter, the corresponding
+;; box-and-pointers structure, and the interpretation of this as a tree
+;; (as in figure 2.6).
+
+(assert-equal '(1 (2 (3 4))) (list 1 (list 2 (list 3 4))))
+(done)
+
+;;
+;;                +---+---+   +---+---+
+;;  (1 (2 (3 4))) | * | *-+-->| * | / |
+;;                +-|-+---+   +-+-+---+
+;;                  |           |
+;;                +-v-+       +---+---+   +---+---+
+;;                | 1 |       | * | *-+-->| * | / |
+;;                +---+       +-|-+---+   +-+-+---+
+;;                              |           |
+;;                            +-v-+       +---+---+   +---+---+
+;;                  (2 (3 4)) | 2 |       | * | *-+-->| * | / |
+;;                            +---+       +-|-+---+   +-+-+---+
+;;                                          |           |
+;;                                        +-v-+       +-v-+
+;;                                  (3 4) | 3 |       | 4 |
+;;                                        +---+       +-+-+
+;;
+;;
+;;                  (1 (2 (3 4)))
+;;                   /\
+;;                  1 (2 (3 4))
+;;                     /\
+;;                    2  (3 4)
+;;                        / \
+;;                       3   4
+;;

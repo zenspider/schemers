7b50e862179bfae0b92081ba620c894d9eae2529
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-17 01:08:30 -0800

    argh. finally activated blank mode in scheme

--- sicp/ch_2/exercise.2.28.scm
+++ sicp/ch_2/exercise.2.28.scm
@@ -2,21 +2,21 @@
 
 (require "../lib/testes.rkt")
 
-;;; Exercise 2.28.  
+;;; Exercise 2.28.
 
 ;; Write a procedure fringe that takes as argument a tree (represented
 ;; as a list) and returns a list whose elements are all the leaves of
 ;; the tree arranged in left-to-right order. For example,
 ;;
 ;; (define x (list (list 1 2) (list 3 4)))
-;; 
+;;
 ;; (fringe x)
 ;; (1 2 3 4)
 ;;
 ;; (fringe (list x x))
 ;; (1 2 3 4 1 2 3 4)
 
-(define (fringe l) 
+(define (fringe l)
   (if (null? l) null
       (append                           ; why append and not cons? :/
        (cond ((null? (car l)) null)

745253958047a1b39291ef0e56da40b551e546ac
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-17 01:09:21 -0800

    LOOK I R A SCHEME CODER

--- sicp/lib/testes.rkt
+++ sicp/lib/testes.rkt
@@ -2,13 +2,43 @@
 
 (provide assert assert-equal done)
 
-(define (assert test)
-  (if test (display ".")
-      (error "failed")))
+;; (define (assert test)
+;;   (if test (display ".")
+;;       (error "failed")))
 
-(define (assert-equal a b)
-  (if (equal? a b) (display ".")
-      (error 'assert-equal "(equal? ~s ~s)~n" a b)))
+; print-test : a macro for printing tests.
+(define-syntax assert
+  (syntax-rules ()
+    ((_ exp) (if test (display ".")
+                 (begin
+                   (display "F: (assert ")
+                   (write (quote exp))
+                   (display ")")
+                   (newline)
+                   (display ";; => ")
+                   (display "(assert ")
+                   (write exp)
+                   (display ")")
+                   (newline))))))
+
+; print-test : a macro for printing tests.
+(define-syntax assert-equal
+  (syntax-rules ()
+    ((_ exp act) (when (not (equal? exp act))
+                   (begin
+                     (display "F: (assert-equal ")
+                     (write (quote exp))
+                     (display " ")
+                     (write (quote act))
+                     (display ")")
+                     (newline)
+                     (display ";; => ")
+                     (display "(assert-equal ")
+                     (write exp)
+                     (display " ")
+                     (write act)
+                     (display ")")
+                     (newline))))))
 
 (define (done)
   (display "done!")

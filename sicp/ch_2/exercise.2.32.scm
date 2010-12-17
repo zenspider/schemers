3778f599d043009a3369e0329a4db2a893ed4cda
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-17 02:16:25 -0800

    2.32

--- sicp/ch_2/exercise.2.32.scm
+++ sicp/ch_2/exercise.2.32.scm
@@ -1,16 +1,35 @@
 #lang racket
 
-;; Exercise 2.32.  
+(require "../lib/testes.rkt")
 
-;; We can represent a set as a list of distinct elements, and we can represent 
-;; the set of all subsets of the set as a list of lists. For example, if the set 
-;; is (1 2 3), then the set of all subsets is 
-;; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3)). Complete the following definition 
-;; of a procedure that generates the set of subsets of a set and give a clear 
-;; explanation of why it works:
+;; Exercise 2.32.
+
+;; We can represent a set as a list of distinct elements, and we can
+;; represent the set of all subsets of the set as a list of lists. For
+;; example, if the set is (1 2 3), then the set of all subsets is
+;; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3)). Complete the following
+;; definition of a procedure that generates the set of subsets of a
+;; set and give a clear explanation of why it works:
 ;;
 ;; (define (subsets s)
 ;;   (if (null? s)
 ;;       (list nil)
 ;;       (let ((rest (subsets (cdr s))))
 ;;         (append rest (map <??> rest)))))
+
+(define (subsets l)
+  (if (null? l) (list l)
+      (let ((rest (subsets (cdr l)))
+            (append-car (lambda (x) (cons (car l) x))))
+        (append rest (map append-car rest)))))
+
+(assert-equal '(()) (subsets '()))
+
+(assert-equal '(() (1))
+              (subsets '(1)))
+
+(assert-equal '(() (2) (1) (1 2))
+              (subsets '(1 2)))
+
+(assert-equal '(() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
+              (subsets '(1 2 3)))

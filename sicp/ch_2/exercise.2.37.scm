579ff3c168cb2770caad01cc82ce9eccaaa4b051
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-21 03:03:02 -0800

    2.37

--- sicp/ch_2/exercise.2.37.scm
+++ sicp/ch_2/exercise.2.37.scm
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "../lib/testes.rkt")
+(require "../lib/utils.rkt")
 
 ;; Exercise 2.37
 
@@ -20,25 +21,22 @@
 ;; operations (which are described in any book on matrix algebra) are
 ;; the following:
 ;;
-;;                                             __
-;;      (dot-product v w)      returns the sum >_i v_i w_i
+;;      (dot-product v w)      returns the sum ∑(i) v[i]*w[i]
 ;;
 ;;      (matrix-*-vector m v)  returns the vector t,
-;;                                         __
-;;                             where t_i = >_j m_(ij) v_j
+;;                             where t[i] = ∑j m[(ij)] v[j]
 ;;
 ;;      (matrix-*-matrix m n)  returns the matrix p,
-;;                                            __
-;;                             where p_(ij) = >_k m_(ik) n_(kj)
+;;                             where p[(ij)] = ∑k m[(ik)] n[(kj)]
 ;;
 ;;      (transpose m)          returns the matrix n,
-;;                             where n_(ij) = m_(ji)
+;;                             where n[(ij)] = m[(ji)]
 ;;
 ;;    We can define the dot product as(4)
-;;
-;;      (define (dot-product v w)
-;;        (accumulate + 0 (map * v w)))
-;;
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
 ;; Fill in the missing expressions in the following procedures for
 ;; computing the other matrix operations. (The procedure
 ;; `accumulate-n' is defined in *Note Exercise 2-36::.)
@@ -53,5 +51,38 @@
 ;;        (let ((cols (transpose n)))
 ;;          (map <??> m)))
 
-;; (assert-equal x y)
+(define (identity-n . x) x)
+
+(define (matrix-*-vector m v)
+  (map (lambda (row) (dot-product v row)) m))
+
+(define (transpose mat)
+  (apply map (cons identity-n mat)))    ; does this make me a bad person?
+
+(define (transpose-slow mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row) (matrix-*-vector cols row)) m)))
+
+(assert-equal 7 (dot-product '(1 2 3) '(-1 1 2)))
+
+;; [ 2 -1 1 ]
+;; [ 0 -2 1 ] * [1 2 3] = [3 -1 -3]
+;; [ 1 -2 0 ]
+
+(define v '(1 2 3))
+(define m '((2 -1 1) (0 -2 1) (1 -2 0)))
+(assert-equal '(3 -1 -3) (matrix-*-vector m v))
+
+(assert-many (lambda (f)
+               (assert-equal '((2 0 1) (-1 -2 -2) (1 1 0)) (f m)))
+             transpose-slow
+             transpose)
+
+;; test data stolen from aja because I'm tired.
+(assert-equal '((19 22) (43 50)) (matrix-*-matrix '((1 2) (3 4))
+                                                  '((5 6) (7 8))))
+
 (done)

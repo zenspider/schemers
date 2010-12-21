057fe8298dd1ebd716101ba0347d33103e98f7ec
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-20 23:05:25 -0800

    Removed accumulate and other tangental functions

--- sicp/ch_2/exercise.2.33.scm
+++ sicp/ch_2/exercise.2.33.scm
@@ -18,36 +18,6 @@
 ;;      (define (length sequence)
 ;;        (accumulate <??> 0 sequence))
 
-(define (accumulate op initial sequence)
-  (if (null? sequence) initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
-
-(define (enumerate-interval low high)
-  (if (> low high)
-      null
-      (cons low (enumerate-interval (+ low 1) high))))
-
-(define (enumerate-tree tree)
-  (cond ((null? tree) null)
-        ((not (pair? tree)) (list tree))
-        (else (append (enumerate-tree (car tree))
-                      (enumerate-tree (cdr tree))))))
-
-(define (sum-odd-squares tree)
-  (accumulate +
-              0
-              (map square
-                   (filter odd?
-                           (enumerate-tree tree)))))
-
-(assert-equal 15             (accumulate + 0 '(1 2 3 4 5)))
-(assert-equal 120            (accumulate * 1 '(1 2 3 4 5)))
-(assert-equal '(1 2 3 4 5)   (accumulate cons null '(1 2 3 4 5)))
-(assert-equal '(2 3 4 5 6 7) (enumerate-interval 2 7))
-(assert-equal '(1 2 3 4 5)   (enumerate-tree '(1 (2 (3 4)) 5)))
-(assert-equal 35             (sum-odd-squares '(1 2 3 4 5)))
-
 (define (my-map p sequence)
   (accumulate (lambda (x y) (cons (p x) y)) null sequence))
 

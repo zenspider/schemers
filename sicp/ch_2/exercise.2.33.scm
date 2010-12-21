565423a9e2ac24b3d7b8e77e0d80a4f7f4859157
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-20 20:14:18 -0800

    2.33

--- sicp/ch_2/exercise.2.33.scm
+++ sicp/ch_2/exercise.2.33.scm
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "../lib/testes.rkt")
+(require "../lib/utils.rkt")
 
 ;; Exercise 2.33
 
@@ -9,7 +10,7 @@
 ;; accumulations:
 ;;
 ;;      (define (map p sequence)
-;;        (accumulate (lambda (x y) <??>) nil sequence))
+;;        (accumulate (lambda (x y) <??>) null sequence))
 ;;
 ;;      (define (append seq1 seq2)
 ;;        (accumulate cons <??> <??>))
@@ -17,5 +18,50 @@
 ;;      (define (length sequence)
 ;;        (accumulate <??> 0 sequence))
 
-;; (assert-equal x y)
+(define (accumulate op initial sequence)
+  (if (null? sequence) initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) null)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+              0
+              (map square
+                   (filter odd?
+                           (enumerate-tree tree)))))
+
+(assert-equal 15             (accumulate + 0 '(1 2 3 4 5)))
+(assert-equal 120            (accumulate * 1 '(1 2 3 4 5)))
+(assert-equal '(1 2 3 4 5)   (accumulate cons null '(1 2 3 4 5)))
+(assert-equal '(2 3 4 5 6 7) (enumerate-interval 2 7))
+(assert-equal '(1 2 3 4 5)   (enumerate-tree '(1 (2 (3 4)) 5)))
+(assert-equal 35             (sum-odd-squares '(1 2 3 4 5)))
+
+(define (my-map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) null sequence))
+
+(define (my-append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (my-length sequence)
+  (accumulate inc 0 sequence))
+
+(assert-equal '(1 4 9) (my-map square '(1 2 3)))
+(assert-equal '(1 2 3 4 5 6) (my-append '(1 2 3) '(4 5 6)))
+(assert-equal 0 (my-length '()))
+(assert-equal 1 (my-length '(1)))
+(assert-equal 2 (my-length '(1 2)))
+(assert-equal 3 (my-length '(1 (2) 3)))
+
 (done)

9703fc252abf599dd3e9eeae411649c4f0b55bb1
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-23 19:42:11 -0800

    2.41

--- sicp/ch_2/exercise.2.41.scm
+++ sicp/ch_2/exercise.2.41.scm
@@ -9,5 +9,28 @@
 ;; integers i, j, and k less than or equal to a given integer n that
 ;; sum to a given integer s.
 
-;; (assert-equal x y)
+(define (sum l) (apply + l))
+
+(define (enumerate-interval low high)
+  (if (> low high) null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+(define (unique-triples n)
+  (flatmap (lambda (i)
+             (flatmap (lambda (j)
+                        (map (lambda (k) (list i j k))
+                             (enumerate-interval 1 (- j 1))))
+                      (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (wtf-do-you-call-this? n s)
+  (filter (lambda (l) (= (sum l) s)) (unique-triples n)))
+
+(assert-equal '((3 2 1) (4 2 1) (4 3 1) (4 3 2)) (unique-triples 4))
+(assert-equal '()                (wtf-do-you-call-this? 4 10))
+(assert-equal '((4 3 2))         (wtf-do-you-call-this? 4 9))
+(assert-equal '((4 3 1) (5 2 1)) (wtf-do-you-call-this? 5 8))
 (done)

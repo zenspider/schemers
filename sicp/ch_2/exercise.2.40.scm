7303176c943de8cbd1c52d08dc1b73ecdf4a42c9
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-23 18:08:58 -0800

    2.40... really?

--- sicp/ch_2/exercise.2.40.scm
+++ sicp/ch_2/exercise.2.40.scm
@@ -10,5 +10,29 @@
 ;; `unique-pairs' to simplify the definition of `prime-sum-pairs'
 ;; given above.
 
+(define (enumerate-interval low high)
+  (if (> low high) null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum (filter prime-sum? (unique-pairs n))))
+
+(assert-equal '((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7)) (prime-sum-pairs 5))
+
 ;; (assert-equal x y)
 (done)

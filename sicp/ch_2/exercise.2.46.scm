463e91152b31674dc580ca8dad9292794dbd4c82
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-26 17:01:20 -0800

    2.46

--- sicp/ch_2/exercise.2.46.scm
+++ sicp/ch_2/exercise.2.46.scm
@@ -18,5 +18,31 @@
 ;;      (x_1, y_1) - (x_2, y_2) = (x_1 - x_2, y_1 - y_2)
 ;;                   s * (x, y) = (sx, sy)
 
+(define make-vect cons)
+(define xcor-vect car)
+(define ycor-vect cdr)
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2))
+             (+ (ycor-vect v1) (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2))
+             (- (ycor-vect v1) (ycor-vect v2))))
+
+(define (scale-vect v x)
+  (make-vect (* (xcor-vect v) x)
+             (* (ycor-vect v) x)))
+
+(let ((v1 (make-vect 3 4))
+      (v2 (make-vect 2 3)))
+  (assert-equal '(3 . 4) v1)
+  (assert-equal 3 (xcor-vect v1))
+  (assert-equal 4 (ycor-vect v1))
+  (assert-equal '(5 . 7) (add-vect v1 v2))
+  (assert-equal '(1 . 1) (sub-vect v1 v2))
+  (assert-equal '(6 . 8) (scale-vect v1 2))
+  (assert-equal '(3/2 . 2) (scale-vect v1 1/2)))
+
 ;; (assert-equal x y)
 (done)

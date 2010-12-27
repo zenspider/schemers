fcf1394356896892f7faf7394804cc8f99257d7e
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-26 16:43:49 -0800

    2.44

--- sicp/ch_2/exercise.2.44.scm
+++ sicp/ch_2/exercise.2.44.scm
@@ -2,6 +2,7 @@
 
 (require "../lib/testes.rkt")
 (require "../lib/utils.rkt")
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
 
 ;; Exercise 2.44
 
@@ -9,5 +10,10 @@
 ;; similar to `right-split', except that it switches the roles of
 ;; `below' and `beside'.
 
-;; (assert-equal x y)
-(done)
+(define (up-split painter n)
+  (if (= n 0) painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+
+;; (paint (up-split einstein 4))
+

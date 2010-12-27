606f2753139bdc1b84a713698f200996060f932f
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-26 17:11:05 -0800

    2.48

--- sicp/ch_2/exercise.2.48.scm
+++ sicp/ch_2/exercise.2.48.scm
@@ -9,10 +9,13 @@
 ;; of vectors--the vector running from the origin to the start-point
 ;; of the segment, and the vector running from the origin to the
 ;; end-point of the segment. Use your vector representation from *Note
-
 ;; Exercise 2-46:: to define a representation for segments with a
 ;; constructor `make-segment' and selectors `start-segment' and
 ;; `end-segment'.
 
-;; (assert-equal x y)
-(done)
+(define make-segment  cons)
+(define start-segment car)
+(define end-segment   cdr)
+
+;; really? do we have to repeat the same shit over and over?
+;; I'm not even gonna write tests for this...

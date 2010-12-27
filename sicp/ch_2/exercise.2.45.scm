5879739f668e836436e0bcca83803e27d558b31a
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-26 16:50:52 -0800

    2.45

--- sicp/ch_2/exercise.2.45.scm
+++ sicp/ch_2/exercise.2.45.scm
@@ -2,6 +2,7 @@
 
 (require "../lib/testes.rkt")
 (require "../lib/utils.rkt")
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
 
 ;; Exercise 2.45
 
@@ -15,5 +16,15 @@
 ;; produces procedures `right-split' and `up-split' with the same
 ;; behaviors as the ones already defined.
 
-;; (assert-equal x y)
-(done)
+(define (split x y)
+  (define (f painter n)
+    (if (= n 0) painter
+        (let ((smaller (f painter (- n 1))))
+          (x painter (y smaller smaller)))))
+  f)
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+;; (paint (up-split einstein 4))
+;; (paint (right-split einstein 4))

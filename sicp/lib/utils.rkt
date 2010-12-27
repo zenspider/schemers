8589b9e9e24f9d2d7865c0f555db85d1aa1822ac
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-26 16:51:20 -0800

    Added enumerate-interval and flatmap

--- sicp/lib/utils.rkt
+++ sicp/lib/utils.rkt
@@ -2,22 +2,27 @@
 
 (provide accumulate
          accumulate-n
+         enumerate-interval
+         flatmap
          fold-left
          fold-right
          inc
          prime?
          square)
 
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs)) null
+      (cons (accumulate   op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define (enumerate-interval low high)
+  (if (> low high) null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
 (define (inc n) (+ 1 n))
-
-(define (square n) (* n n))
-
-(define (fold-right op initial sequence)
-  (if (null? sequence) initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
-
-(define accumulate fold-right)
 
 (define (fold-left op initial sequence)
   (define (iter result rest)
@@ -27,10 +32,12 @@
               (cdr rest))))
   (iter initial sequence))
 
-(define (accumulate-n op init seqs)
-  (if (null? (car seqs)) null
-      (cons (accumulate   op init (map car seqs))
-            (accumulate-n op init (map cdr seqs)))))
+(define (fold-right op initial sequence)
+  (if (null? sequence) initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define accumulate fold-right)
 
 (define (prime? n)
   (define (smallest-divisor n)
@@ -45,30 +52,4 @@
       (find-divisor n 2))
   (= n (smallest-divisor n)))
 
-;; (define (flatmap proc seq)
-;;   (accumulate append null (map proc seq)))
-;; (define (enumerate-interval low high)
-;;   (if (> low high)
-;;       null
-;;       (cons low (enumerate-interval (+ low 1) high))))
-;;
-;; (define (enumerate-tree tree)
-;;   (cond ((null? tree) null)
-;;         ((not (pair? tree)) (list tree))
-;;         (else (append (enumerate-tree (car tree))
-;;                       (enumerate-tree (cdr tree))))))
-;;
-;; (define (sum-odd-squares tree)
-;;   (accumulate +
-;;               0
-;;               (map square
-;;                    (filter odd?
-;;                            (enumerate-tree tree)))))
-;;
-;; (assert-equal 15             (accumulate + 0 '(1 2 3 4 5)))
-;; (assert-equal 120            (accumulate * 1 '(1 2 3 4 5)))
-;; (assert-equal '(1 2 3 4 5)   (accumulate cons null '(1 2 3 4 5)))
-;; (assert-equal '(2 3 4 5 6 7) (enumerate-interval 2 7))
-;; (assert-equal '(1 2 3 4 5)   (enumerate-tree '(1 (2 (3 4)) 5)))
-;; (assert-equal 35             (sum-odd-squares '(1 2 3 4 5)))
-
+(define (square n) (* n n))

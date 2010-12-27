e8437b00981cf49ebfb010e6eabf46f21dd934db
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-26 17:08:11 -0800

    2.47

--- sicp/ch_2/exercise.2.47.scm
+++ sicp/ch_2/exercise.2.47.scm
@@ -16,5 +16,28 @@
 ;; For each constructor supply the appropriate selectors to produce an
 ;; implementation for frames.
 
-;; (assert-equal x y)
+(define (make-frame1 origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (make-frame2 origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define origin-frame1 car)
+(define edge1-frame1 cadr)
+(define edge2-frame1 caddr)
+
+(define origin-frame2 car)
+(define edge1-frame2 cadr)
+(define edge2-frame2 cddr)
+
+(let ((f1 (make-frame1 '(0 . 0) '(1 . 0) '(0 . 2)))
+      (f2 (make-frame2 '(0 . 0) '(1 . 0) '(0 . 2))))
+  (assert-equal '(0 . 0) (origin-frame1 f1))
+  (assert-equal '(1 . 0) (edge1-frame1 f1))
+  (assert-equal '(0 . 2) (edge2-frame1 f1))
+
+  (assert-equal '(0 . 0) (origin-frame2 f2))
+  (assert-equal '(1 . 0) (edge1-frame2 f2))
+  (assert-equal '(0 . 2) (edge2-frame2 f2)))
+
 (done)

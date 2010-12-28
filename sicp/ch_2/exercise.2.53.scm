0a5cd75aa47762683aa8c960d915b2b86a936389
Ryan Davis <ryand-ruby@zenspider.com>
2010-12-27 20:25:23 -0800

    2.53

--- sicp/ch_2/exercise.2.53.scm
+++ sicp/ch_2/exercise.2.53.scm
@@ -1,5 +1,8 @@
 #lang racket
 
+(require "../lib/testes.rkt")
+(require "../lib/utils.rkt")
+
 ;;; Exercise 2.53:
 
 ;; What would the interpreter print in response to evaluating each of
@@ -18,3 +21,13 @@
 ;;      (memq 'red '((red shoes) (blue socks)))
 ;;
 ;;      (memq 'red '(red shoes blue socks))
+
+(assert-equal '(a b c)                (list 'a 'b 'c))
+(assert-equal '((george))             (list (list 'george)))
+(assert-equal '((y1 y2))              (cdr '((x1 x2) (y1 y2))))
+(assert-equal '(y1 y2)                (cadr '((x1 x2) (y1 y2))))
+(assert-equal #f                      (pair? (car '(a short list))))
+(assert-equal #f                      (memq 'red '((red shoes) (blue socks))))
+(assert-equal '(red shoes blue socks) (memq 'red '(red shoes blue socks)))
+
+(done)

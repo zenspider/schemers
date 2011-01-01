e854349168e7339b649aae3a0c218d8f1f495e76
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-01 14:48:25 -0800

    2.59

--- sicp/ch_2/exercise.2.59.scm
+++ sicp/ch_2/exercise.2.59.scm
@@ -8,5 +8,18 @@
 ;; Implement the `union-set' operation for the unordered-list
 ;; representation of sets.
 
-;; (assert-equal x y)
+(define (union-set s1 s2)
+  (if (null? s2) s1
+      (union-set (adjoin-set (car s2) s1) (cdr s2))))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set) set
+      (cons x set)))
+
+(assert-equal '(1 2 3) (sort-< (union-set '(3) '(1 2))))
 (done)

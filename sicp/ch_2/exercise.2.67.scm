aef3585faa4c3871a80ee96391fa47025a417f90
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-01 19:36:35 -0800

    refactored to lib/huffman.rkt

--- sicp/ch_2/exercise.2.67.scm
+++ sicp/ch_2/exercise.2.67.scm
@@ -19,43 +19,7 @@
 ;; Use the `decode' procedure to decode the message, and give the
 ;; result.
 
-(define (make-leaf symbol weight) (list 'leaf symbol weight))
-(define (leaf? object) (eq? (car object) 'leaf))
-(define symbol-leaf cadr)
-(define weight-leaf caddr)
-
-(define (symbols tree)
-  (if (leaf? tree)
-      (list (symbol-leaf tree))
-      (caddr tree)))
-
-(define (weight tree)
-  (if (leaf? tree)
-      (weight-leaf tree)
-      (cadddr tree)))
-
-(define (make-code-tree left right)
-  (list left
-        right
-        (append (symbols left) (symbols right))
-        (+ (weight left) (weight right))))
-
-(define left-branch car)
-(define right-branch cadr)
-
-(define (choose-branch bit branch)
-  (cond ((= bit 0) (left-branch branch))
-        ((= bit 1) (right-branch branch))
-        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
-
-(define (decode bits tree)
-  (define (decode-1 bits current-branch)
-    (if (null? bits) null
-        (let ((next-branch (choose-branch (car bits) current-branch)))
-          (if (leaf? next-branch)
-              (cons (symbol-leaf next-branch) (decode-1 (cdr bits) tree))
-              (decode-1 (cdr bits) next-branch)))))
-  (decode-1 bits tree))
+(require "../lib/huffman.rkt")
 
 (define sample-tree
   (make-code-tree (make-leaf 'A 4)
@@ -63,7 +27,6 @@
                    (make-leaf 'B 2)
                    (make-code-tree (make-leaf 'D 1)
                                    (make-leaf 'C 1)))))
-
 ;;     *
 ;;    / \
 ;;   A   *

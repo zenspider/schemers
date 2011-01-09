deae652602a5a7b4802e21b6483e38628ff2c84d
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-08 16:27:33 -0800

    2.73

--- sicp/ch_2/exercise.2.73.scm
+++ sicp/ch_2/exercise.2.73.scm
@@ -2,6 +2,7 @@
 
 (require "../lib/testes.rkt")
 (require "../lib/utils.rkt")
+;; (require "../lib/table.rkt")
 
 ;;; Exercise 2.73
 
@@ -29,29 +30,55 @@
 ;; and the operation being performed is `deriv'.  We can transform
 ;; this program into data-directed style by rewriting the basic
 ;; derivative procedure as
+
+;; (define (deriv exp var)
+;;   (cond ((number? exp) 0)
+;;         ((variable? exp) (if (same-variable? exp var) 1 0))
+;;         (else ((get 'deriv (operator exp)) (operands exp)
+;;                var))))
 ;;
-;;      (define (deriv exp var)
-;;         (cond ((number? exp) 0)
-;;               ((variable? exp) (if (same-variable? exp var) 1 0))
-;;               (else ((get 'deriv (operator exp)) (operands exp)
-;;                                                  var))))
+;; (define (operator exp) (car exp))
 ;;
-;;      (define (operator exp) (car exp))
-;;
-;;      (define (operands exp) (cdr exp))
-;;
+;; (define (operands exp) (cdr exp))
+
 ;;   a. Explain what was done above.  Why can't we assimilate the
 ;;      predicates `number?' and `same-variable?' into the
 ;;      data-directed dispatch?
-;;
+
+;; A: number? and same-variable? aren't datatype specific so it
+;;    doesn't make sense to have them as an operator on deriv
+
 ;;   b. Write the procedures for derivatives of sums and products,
 ;;      and the auxiliary code required to install them in the table
 ;;      used by the program above.
+
+;; (define deriv-table (make-table))
+;; (define get (deriv-table 'lookup-proc))
+;; (define put (deriv-table 'insert-proc!))
 ;;
+;; (put 'deriv '+ (lambda (operands var)
+;;                  (make-sum (deriv (addend exp) var)
+;;                            (deriv (augend exp) var))))
+;;
+;; (put 'deriv '* (lambda (operands var)
+;;                  (make-sum
+;;                   (make-product (multiplier exp)
+;;                                 (deriv (multiplicand exp) var))
+;;                   (make-product (deriv (multiplier exp) var)
+;;                                 (multiplicand exp)))))
+
 ;;   c. Choose any additional differentiation rule that you like,
 ;;      such as the one for exponents (*Note Exercise 2-56::), and
 ;;      install it in this data-directed system.
-;;
+
+;; (put 'deriv '** (lambda (operands var)
+;;                   (make-product
+;;                    (make-product
+;;                     (exponent exp)
+;;                     (make-exponentiation (base exp)
+;;                                          (make-sum (exponent exp) -1)))
+;;                    (deriv (base exp) var))))
+
 ;;   d. In this simple algebraic manipulator the type of an
 ;;      expression is the algebraic operator that binds it together.
 ;;      Suppose, however, we indexed the procedures in the opposite
@@ -62,5 +89,6 @@
 ;;      What corresponding changes to the derivative system are
 ;;      required?
 
-;; (assert-equal x y)
-(done)
+;; A: none. you just have to have your put's reversed as well.
+
+;; TODO: maybe... get this actually running once we bypass set-cdr!

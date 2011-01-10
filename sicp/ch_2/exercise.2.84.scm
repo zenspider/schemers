221877cbe515f771a26ed280cd2aebb2c7ccfa5f
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-09 16:54:40 -0800

    2.84

--- sicp/ch_2/exercise.2.84.scm
+++ sicp/ch_2/exercise.2.84.scm
@@ -13,5 +13,24 @@
 ;; a manner that is "compatible" with the rest of the system and will
 ;; not lead to problems in adding new levels to the tower.
 
-;; (assert-equal x y)
-(done)
+(define tower '(integer rational real complex))
+
+(define (subtype? x y)
+  (and (not (equal? x y))
+       (let ((supers (member x tower)))
+         (and supers (member y supers)))))
+
+(assert-equal '(complex) (subtype? 'integer 'complex))
+(assert-equal #f         (subtype? 'complex 'integer))
+(assert-equal #f         (subtype? 'integer 'integer))
+(assert-equal #f         (subtype? 'complex 'complex))
+
+;; (define (raise-from-to x y)
+;;   (if (subtype? (tag-type x) (tag-type y))
+;;       (raise-from-to (raise x) y)
+;;       x))
+
+;; the rest is just hooking it in. Unfortunately the direction I took
+;; with my last converter was too clever to make that a simple pop-in.
+
+;; TODO: make this work at some point

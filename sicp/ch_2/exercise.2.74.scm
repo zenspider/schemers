7185f84be96a8a1dfbad42afe3367f3a4e252847
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-09 17:21:49 -0800

    2.74

--- sicp/ch_2/exercise.2.74.scm
+++ sicp/ch_2/exercise.2.74.scm
@@ -35,23 +35,62 @@
 ;;      division's file.  Explain how the individual divisions' files
 ;;      should be structured.  In particular, what type information
 ;;      must be supplied?
-;;
+
+(define db
+  '(((x fred) (ssn 123) (salary 24))
+    ((y sue) (ssn 456) (salary 42))))
+
+(define (get-record dept name)
+  (let ((id (list dept name)))
+    (define (iterate db)
+      (if (null? db) null
+          (if (equal? id (caar db))
+              (cdar db)
+              (iterate (cdr db)))))
+    (iterate db)))
+
+(assert-equal '((ssn 123) (salary 24)) (get-record 'x 'fred))
+(assert-equal '((ssn 456) (salary 42)) (get-record 'y 'sue))
+(assert-equal null  (get-record 'z 1))
+
 ;;   b. Implement for headquarters a `get-salary' procedure that
 ;;      returns the salary information from a given employee's record
 ;;      from any division's personnel file.  How should the record be
 ;;      structured in order to make this operation work?
-;;
+
+;; yes, I used assoc. sue me. It takes 4 lines to write one of your own.
+
+(define (get-salary dept name)
+  (let ((record (get-record dept name)))
+    (and record (assoc 'salary record))))
+
+(assert-equal '(salary 24) (get-salary 'x 'fred))
+
 ;;   c. Implement for headquarters a `find-employee-record'
 ;;      procedure.  This should search all the divisions' files for
 ;;      the record of a given employee and return the record.  Assume
 ;;      that this procedure takes as arguments an employee's name and
 ;;      a list of all the divisions' files.
-;;
+
+;; huh? what's the difference between this and get-record? The unique
+;; id for a record is the dept and name, so pretending that the name
+;; will be unique now is a really bad idea!
+
+(define (find-employee-record name db)
+  (define (iterate db)
+    (if (null? db) null
+        (if (equal? name (cadaar db))
+            (car db)
+            (iterate (cdr db)))))
+  (iterate db))
+
+(assert-equal (car db) (find-employee-record 'fred db))
+
 ;;   d. When Insatiable takes over a new company, what changes must
 ;;      be made in order to incorporate the new personnel information
 ;;      into the central system?
 
-;; TODO
+;; A: the "dept" tag needs to be unique, that's it... ignoring
+;; find-employee-record's horrible concept
 
-;; (assert-equal x y)
 (done)

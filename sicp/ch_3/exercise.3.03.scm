143dc3849840b42895f3138d57a78efea7e1dec0
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-15 14:30:22 -0800

    3.03

--- sicp/ch_3/exercise.3.03.scm
+++ sicp/ch_3/exercise.3.03.scm
@@ -21,5 +21,31 @@
 ;;      ((acc 'some-other-password 'deposit) 50)
 ;;      "Incorrect password"
 
-;; (assert-equal x y)
+(define (make-account balance password)
+  (define (bad _) "Incorrect password")
+
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin
+          (set! balance (- balance amount))
+          balance)
+        "Issufficient funds: "))
+
+  (define (dispatch p m)
+    (if (eq? p password)
+        (cond ((eq? 'withdraw m) withdraw)
+              ((eq? 'deposit  m) deposit)
+              (else (error "Unknown message: " m)))
+        bad))
+  dispatch)
+
+(define acc (make-account 100 'secret-password))
+
+(assert-equal 60 ((acc 'secret-password 'withdraw) 40))
+(assert-equal 70 ((acc 'secret-password 'deposit) 10))
+(assert-equal "Incorrect password" ((acc 'some-other-password 'deposit) 50))
 (done)

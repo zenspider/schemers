61bcb1517d2c1237be4243fd6c04e23b34c7b079
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-16 00:14:49 -0800

    3.07

--- sicp/ch_3/exercise.3.07.scm
+++ sicp/ch_3/exercise.3.07.scm
@@ -25,5 +25,48 @@
 ;; `paul-acc' and the password `rosebud'.  You may wish to modify your
 ;; solution to *Note Exercise 3-3:: to accommodate this new feature
 
-;; (assert-equal x y)
+(define (make-account balance passwords)
+  (define (bad _) "Incorrect password")
+
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin
+          (set! balance (- balance amount))
+          balance)
+        "Issufficient funds: "))
+
+  ;; This is a stupid simple way to do it. I don't think the banks
+  ;; would ever model it this way. Another way would be to make a
+  ;; make-ledger function that returns a shared lambda between them.
+  ;; Then an account is just a password system and delegates
+  ;; everything to the ledger.
+
+  (set! passwords (list passwords))     ; hah!
+  (define (add-password password)
+    (set! passwords (cons password passwords)))
+
+  (define (dispatch p m)
+    (if (member p passwords)
+        (cond ((eq? m 'withdraw) withdraw)
+              ((eq? m 'deposit) deposit)
+              ((eq? m 'add-password) add-password)
+              (else (error "Unknown message: " m)))
+        bad))
+
+  dispatch)
+
+(define (make-joint account password new-password)
+  ((account password 'add-password) new-password)
+  account)
+
+(define peter-acc (make-account 100 'open-sesame))
+(define paul-acc  (make-joint peter-acc 'open-sesame 'rosebud))
+
+(assert-equal 90 ((peter-acc 'open-sesame 'withdraw) 10))
+(assert-equal 80 ((paul-acc  'rosebud     'withdraw) 10))
+(assert-equal 70 ((peter-acc 'open-sesame 'withdraw) 10))
 (done)

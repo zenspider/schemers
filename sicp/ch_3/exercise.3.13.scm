00b4cd127a3f0ee6ca9f4b960833282cf11cdb33
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-29 22:53:22 -0800

    3.13

--- sicp/ch_3/exercise.3.13.scm
+++ sicp/ch_3/exercise.3.13.scm
@@ -3,23 +3,36 @@
 (import testes)
 (require 'myutils)
 (import myutils)
+(require-extension test)
 
 ;;; Exercise 3.13
 
 ;; Consider the following `make-cycle' procedure,
 ;; which uses the `last-pair' procedure defined in *Note Exercise
 ;; 3-12:::
-;;
-;;      (define (make-cycle x)
-;;        (set-cdr! (last-pair x) x)
-;;        x)
-;;
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
 ;; Draw a box-and-pointer diagram that shows the structure `z'
 ;; created by
-;;
-;;      (define z (make-cycle (list 'a 'b 'c)))
-;;
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+;;     +--------+
+;;     v        |
+;; z = *--*--*--+
+;;     |  |  |
+;;     a  b  c
+
 ;; What happens if we try to compute `(last-pair z)'?
 
-;; (assert-equal x y)
+;; A: infinite loop...
+
 (done)

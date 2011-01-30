82e6db02f0ee2a3f5f288e3ab9293d0117e07914
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-29 22:05:57 -0800

    3.12

--- sicp/ch_3/exercise.3.12.scm
+++ sicp/ch_3/exercise.3.12.scm
@@ -6,57 +6,69 @@
 
 ;;; Exercise 3.12
 
-;; The following procedure for appending lists was
-;; introduced in section *Note 2-2-1:::
-;;
-;;      (define (append x y)
-;;        (if (null? x)
-;;            y
-;;            (cons (car x) (append (cdr x) y))))
-;;
-;; `Append' forms a new list by successively `cons'ing the elements of
-;; `x' onto `y'.  The procedure `append!' is similar to `append', but
-;; it is a mutator rather than a constructor.  It appends the lists
-;; by splicing them together, modifying the final pair of `x' so that
-;; its `cdr' is now `y'.  (It is an error to call `append!' with an
-;; empty `x'.)
-;;
-;;      (define (append! x y)
-;;        (set-cdr! (last-pair x) y)
-;;        x)
-;;
-;; Here `last-pair' is a procedure that returns the last pair in its
-;; argument:
-;;
-;;      (define (last-pair x)
-;;        (if (null? (cdr x))
-;;            x
-;;            (last-pair (cdr x))))
-;;
-;; Consider the interaction
-;;
-;;      (define x (list 'a 'b))
-;;
-;;      (define y (list 'c 'd))
-;;
-;;      (define z (append x y))
-;;
-;;      z
-;;      (a b c d)
-;;
-;;      (cdr x)
-;;      <RESPONSE>
-;;
-;;      (define w (append! x y))
-;;
-;;      w
-;;      (a b c d)
-;;
-;;      (cdr x)
-;;      <RESPONSE>
-;;
-;; What are the missing <RESPONSE>s?  Draw box-and-pointer diagrams to
-;; explain your answer.
+;; The following procedure for appending lists was introduced in
+;; section *Note 2-2-1:::
 
-;; (assert-equal x y)
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+;; `Append' forms a new list by successively `cons'ing the elements of
+;; `x' onto `y'. The procedure `append!' is similar to `append', but
+;; it is a mutator rather than a constructor. It appends the lists by
+;; splicing them together, modifying the final pair of `x' so that its
+;; `cdr' is now `y'. (It is an error to call `append!' with an empty
+;; `x'.)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+;; Consider the interaction
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+
+(assert-equal '(a b c d) z)
+(assert-equal '(b) (cdr x))
+
+;; x = *--*--0
+;;     |  |
+;;     a  b
+;;
+;; (cdr x) = *--0
+;;           |
+;;           b
+
+(define w (append! x y))
+(assert-equal '(a b c d) w)
+(assert-equal '(b c d) (cdr x))
+
+;; x = *--*--0
+;;     |  |
+;;     a  b
+;;
+;; y = *--*--0
+;;     |  |
+;;     c  d
+;;
+;; (append! x y)
+;; (set-cdr! (last-pair x) y)
+;; (set-cdr! (cdr x) y)
+;;
+;; x = *--*--*--*--0
+;;     |  |  |  |
+;;     a  b  c  d
+;;
+;; (cdr x) = *--*--*--*--0
+;;           |  |  |  |
+;;           a  b  c  d
+
 (done)

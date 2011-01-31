651931f09f2129b90b9a1c4856658cb136a0dc60
Ryan Davis <ryand-ruby@zenspider.com>
2011-01-30 23:56:02 -0800

    3.17

--- sicp/ch_3/exercise.3.17.scm
+++ sicp/ch_3/exercise.3.17.scm
@@ -3,6 +3,7 @@
 (import testes)
 (require 'myutils)
 (import myutils)
+(require-extension test)
 
 ;;; Exercise 3.17
 
@@ -12,5 +13,30 @@
 ;; maintaining an auxiliary data structure that is used to keep track
 ;; of which pairs have already been counted.)
 
-;; (assert-equal x y)
-(done)
+(define (count-pairs x)
+  (let ((visited '()))
+    (define (iterate l)
+      (define (count f)
+        (let ((cell (f l)))
+          (if (or (memq cell visited) (not (pair? cell))) 0
+              (begin
+                (set! visited (cons cell visited))
+                (+ 1 (iterate cell))))))
+      (if (null? l) 0
+          (+ (count car) (count cdr))))
+    (+ 1 (iterate x))))
+
+(define a '(a))
+(define b (cons a a))
+(define c (cons b b))
+
+(define x '(a b c))
+(define y (cons 'b b))
+(define z c)
+
+(test-group "3.17"
+            (test 1 (count-pairs '()))
+            (test 3 (count-pairs x))
+            (test 3 (count-pairs y))
+            (test 3 (count-pairs z)))
+

c91aa5811d3efc530ad2a0ebdce882ffcb3cd1ce
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-06 17:50:00 -0800

    3.27

--- sicp/ch_3/exercise.3.27.scm
+++ sicp/ch_3/exercise.3.27.scm
@@ -1,8 +1,5 @@
 
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
+(use test)
 
 ;;; Exercise 3.27
 
@@ -18,38 +15,96 @@
 ;; the ordinary way and stores this in the table.  As an example of
 ;; memoization, recall from section *Note 1-2-2:: the exponential
 ;; process for computing Fibonacci numbers:
-;;
-;;      (define (fib n)
-;;        (cond ((= n 0) 0)
-;;              ((= n 1) 1)
-;;              (else (+ (fib (- n 1))
-;;                       (fib (- n 2))))))
-;;
-;; The memoized version of the same procedure is
-;;
-;;      (define memo-fib
-;;        (memoize (lambda (n)
-;;                   (cond ((= n 0) 0)
-;;                         ((= n 1) 1)
-;;                         (else (+ (memo-fib (- n 1))
-;;                                  (memo-fib (- n 2))))))))
-;;
-;; where the memoizer is defined as
-;;
-;;      (define (memoize f)
-;;        (let ((table (make-table)))
-;;          (lambda (x)
-;;            (let ((previously-computed-result (lookup x table)))
-;;              (or previously-computed-result
-;;                  (let ((result (f x)))
-;;                    (insert! x result table)
-;;                    result))))))
-;;
-;; Draw an environment diagram to analyze the computation of
-;; `(memo-fib 3)'.  Explain why `memo-fib' computes the nth Fibonacci
-;; number in a number of steps proportional to n.  Would the scheme
-;; still work if we had simply defined `memo-fib' to be `(memoize
-;; fib)'?
 
-;; (assert-equal x y)
-(done)
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+;; With the memoizer defined as:
+
+(define (make-table . test)
+  (let ((local-table (list '*table*))
+        (same-key? (optional test equal?))) ;; see note below
+
+    (define (lookup key)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+            (cdr record)
+            #f)))
+
+    (define (assoc key records)
+      (cond ((null? records) #f)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+
+    (define (insert key value)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+            (set-cdr! record value)
+            (set-cdr! local-table (cons (cons key value) (cdr local-table))))))
+
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc) insert)))
+
+    dispatch))
+
+(define (lookup  k t)   ((t 'lookup-proc) k))
+(define (insert! k v t) ((t 'insert-proc) k v))
+
+(define (memoize f)
+  (let ((table (make-table)))
+    (lambda (x)
+      (let ((previously-computed-result (lookup x table)))
+        (or previously-computed-result
+            (let ((result (f x)))
+              (insert! x result table)
+              result))))))
+
+;; The memoized version of the same procedure is:
+
+(define memo-fib
+  (memoize (lambda (n)
+             (cond ((= n 0) 0)
+                   ((= n 1) 1)
+                   (else (+ (memo-fib (- n 1))
+                            (memo-fib (- n 2))))))))
+
+;; Draw an environment diagram to analyze the computation of
+;; `(memo-fib 3)'.
+
+;; E0:
+;;   fib: ...
+;;   memoize: ...
+;;   memo-fib: E1
+;;
+;; E1:
+;;   f: ...
+;;   table: *-------*---*---*--0
+;;          |       |   |   |
+;;          *table* 0:0 1:1 2:2
+
+;; Explain why `memo-fib' computes the nth Fibonacci number in a
+;; number of steps proportional to n.
+
+;; A: it is always computing (memo-fib N) as (get (- N 2)) + (get (- N 1))
+
+;; Would the scheme still work if we had simply defined `memo-fib' to
+;; be `(memoize fib)'?
+
+;; A: not as written, since the computation needs to recurse on itself.
+
+(test-group "3.27"
+            (test 6765 (fib 20))
+            (test 6765 (memo-fib 20)))
+
+;; (fib 30) vs (memo-fib 30):
+;; procedure     calls  seconds  average  percent
+;; ----------------------------------------------
+;; fib         2692537    6.958    0.000  100.000
+;; lookup           59    0.000    0.000    0.000
+;; insert!          31    0.000    0.000    0.000
+;; memoize           1    0.000    0.000    0.000
+;; make-table        1    0.000    0.000    0.000

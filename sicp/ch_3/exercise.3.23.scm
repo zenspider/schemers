9f6f4972620e23aea8e59b4f687df42efd63b225
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-06 16:50:41 -0800

    3.23

--- sicp/ch_3/exercise.3.23.scm
+++ sicp/ch_3/exercise.3.23.scm
@@ -1,8 +1,5 @@
 
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
+(require-extension test)
 
 ;;; Exercise 3.23
 
@@ -16,5 +13,120 @@
 ;; and give implementations of the operations.(2)  All operations
 ;; should be accomplished in [theta](1) steps.
 
-;; (assert-equal x y)
-(done)
+(define null '())
+
+;; head         tail
+;; |            |
+;; [*|*] [*|*] [*|*]
+;;  | |   | |   | |
+;;  a |   b |   c |
+;;    |     |     |
+;;   [0|b] [a|c] [b|0]
+
+(define (make-deque)
+  (let ((head '())
+        (tail '()))
+
+    (define (empty?)
+      (null? head))
+
+    ;; unshift a cell onto the front of the deque
+    (define (unshift x)
+      (if (empty?)
+          (let ((cell (cons x (list null null))))
+            (set! head cell)
+            (set! tail cell))
+          (let ((cell (cons x (list null head))))
+            (set-car! (cdr head) cell)
+            (set! head cell))))
+
+    ;; shift a cell from the front of the deque
+    (define (shift)
+      (cond ((empty?)
+             (error "DELETE! called with an empty queue" head))
+            (else
+             (set! head (cddr head))
+             (if (null? head)
+                 (set! tail null)
+                 (set-car! (cdr head) null)))))
+
+    ;; push a cell onto the end of the deque
+    (define (push x)
+      (if (empty?)
+          (let ((cell (cons x (list null null))))
+            (set! head cell)
+            (set! tail cell))
+          (let ((cell (cons x (list tail null))))
+            (set-cdr! (cdr tail) cell)
+            (set! tail cell))))
+
+    ;; pop a cell from the end of the deque
+    (define (pop)
+      (cond ((empty?)
+             (error "DELETE! called with an empty queue" head))
+            (else
+             (set! tail (cadr tail))
+             (if (null? tail)
+                 (set! head null)
+                 (set-cdr! (cdr tail) null)))))
+
+    (define (dispatch m)
+      (cond ((eq? m 'head)    head)
+            ((eq? m 'tail)    tail)
+            ((eq? m 'empty?)  empty?)
+            ((eq? m 'unshift) unshift)
+            ((eq? m 'push)    push)
+            ((eq? m 'shift)   shift)
+            ((eq? m 'pop)     pop)
+            (else (error "bad msg: " m))))
+
+    dispatch))
+
+(define (head-deque q)            (q 'head))
+(define (tail-deque q)            (q 'tail))
+(define (empty-deque? q)         ((q 'empty?)))
+(define (head-insert-deque! q x) ((q 'unshift) x))
+(define (tail-insert-deque! q x) ((q 'push) x))
+(define (head-delete-deque! q)   ((q 'shift)))
+(define (tail-delete-deque! q)   ((q 'pop)))
+
+(test-group "3.23"
+            (define q (make-deque))
+
+            ;; ()
+            (test #t (empty-deque? q))
+            (test null (head-deque q))
+            (test null (tail-deque q))
+
+            ;; (a)
+            (head-insert-deque! q 'a)
+            (test 'a (car (head-deque q)))
+            (test 'a (car (tail-deque q)))
+
+            ;; (a b)
+            (tail-insert-deque! q 'b)
+            (test 'a (car (head-deque q)))
+            (test 'b (car (tail-deque q)))
+
+            ;; (a b c)
+            (display-deque q)
+            (newline)
+            (tail-insert-deque! q 'c)
+            (test 'a (car (head-deque q)))
+            (test 'c (car (tail-deque q)))
+
+            ;; (b c)
+            (head-delete-deque! q)
+            (test 'b (car (head-deque q)))
+            (test 'c (car (tail-deque q)))
+
+            ;; (b)
+            (tail-delete-deque! q)
+            (test 'b (car (head-deque q)))
+            (test 'b (car (tail-deque q)))
+
+            ;; ()
+            (head-delete-deque! q)
+            (test #t (empty-deque? q))
+            (test null (head-deque q))
+            (test null (tail-deque q)))

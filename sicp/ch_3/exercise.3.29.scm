1dfbfeec71af61205780d2712869282363fb4b9d
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-08 02:08:02 -0800

    3.29

--- sicp/ch_3/exercise.3.29.scm
+++ sicp/ch_3/exercise.3.29.scm
@@ -1,16 +1,31 @@
 
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
-
 ;;; Exercise 3.29
 
-;; Another way to construct an or-gate is as a
-;; compound digital logic device, built from and-gates and inverters.
-;; Define a procedure `or-gate' that accomplishes this.  What is the
-;; delay time of the or-gate in terms of `and-gate-delay' and
-;; `inverter-delay'?
+;; Another way to construct an or-gate is as a compound digital logic
+;; device, built from and-gates and inverters. Define a procedure
+;; `or-gate' that accomplishes this.
 
-;; (assert-equal x y)
-(done)
+;; A B A|B
+;; 0 0 0
+;; 0 1 1
+;; 1 0 1
+;; 1 1 1
+;;
+;; or: !(!A & !B)
+;; or: ->-|\
+;;        | )->-
+;;     ->-|/
+
+(define (or-gate a b output)
+  (let ((c (make-wire))
+        (d (make-wire))
+        (e (make-wire)))
+    (inverter a c)
+    (inverter b d)
+    (and-gate c d e)
+    (inverter e output)))
+
+;; What is the delay time of the or-gate in terms of `and-gate-delay'
+;; and `inverter-delay'?
+
+;; A: 2i + a. See diagram above for obvious reason.

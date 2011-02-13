f6fbb4ebeff9787c2973eef6f637c544dd7ac2b0
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-12 18:23:17 -0800

    3.34

--- sicp/ch_3/exercise.3.34.scm
+++ sicp/ch_3/exercise.3.34.scm
@@ -1,8 +1,7 @@
 
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
+(use test)
+(require 'constraints)
+(import constraints)
 
 ;;; Exercise 3.34
 
@@ -11,11 +10,31 @@
 ;; connector `b' on the second terminal will always be the square of
 ;; the value `a' on the first terminal.  He proposes the following
 ;; simple device made from a multiplier:
-;;
-;;      (define (squarer a b)
-;;        (multiplier a a b))
-;;
+
+(define (squarer a b)
+  (multiplier a a b))
+
 ;; There is a serious flaw in this idea.  Explain.
 
-;; (assert-equal x y)
-(done)
+(test-group "3.34"
+            (define A (make-connector))
+            (define B (make-connector))
+            (define C (make-connector))
+
+            (squarer A B)
+            (squarer B C)
+
+            ;; Honestly... I'm not seeing it.
+
+            (set-value! A 2 'user)
+            (test  4 (get-value B))
+            (test 16 (get-value C))
+
+            (forget-value! A 'user)
+            (forget-value! B 'user)
+            (forget-value! C 'user)
+
+            (set-value! C 16 'user)
+            (test 4 (get-value B))
+            (test 2 (get-value A)))
+

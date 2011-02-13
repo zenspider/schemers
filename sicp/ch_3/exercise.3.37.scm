23a1482da37cbe757cef7ad4ac14372675541e80
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-12 18:50:32 -0800

    3.37

--- sicp/ch_3/exercise.3.37.scm
+++ sicp/ch_3/exercise.3.37.scm
@@ -1,8 +1,8 @@
 
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
+(use test)
+(use numbers)
+(require 'constraints)
+(import constraints)
 
 ;;; Exercise 3.37
 
@@ -22,15 +22,39 @@
 ;; arithmetic operations.  For example, `c+' takes two connectors as
 ;; arguments and returns a connector that is related to these by an
 ;; adder constraint:
-;;
-;;      (define (c+ x y)
-;;        (let ((z (make-connector)))
-;;          (adder x y z)
-;;          z))
-;;
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
 ;; Define analogous procedures `c-', `c*', `c/', and `cv' (constant
 ;; value) that enable us to define compound constraints as in the
 ;; converter example above.(3)
 
-;; (assert-equal x y)
-(done)
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier y z x)
+    z))
+
+(define (cv val)
+  (let ((temp (make-connector)))
+    (set-value! temp val 'user)
+    temp))
+
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+(define C (make-connector))
+(define F (celsius-fahrenheit-converter C))
+
+(test-group "3.37"
+            (set-value! F 212 'user)
+            (test 100.0 (get-value C)))

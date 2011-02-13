864003752493b1321892baab10312748cf4c926b
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-12 18:23:18 -0800

    3.33

--- sicp/ch_3/exercise.3.33.scm
+++ sicp/ch_3/exercise.3.33.scm
@@ -1,8 +1,7 @@
 
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
+(use test)
+(require 'constraints)
+(import constraints)
 
 ;;; Exercise 3.33
 
@@ -12,5 +11,29 @@
 ;; constraint that the value of `c' is the average of the values of
 ;; `a' and `b'.
 
-;; (assert-equal x y)
-(done)
+(define (averager m n avg)
+  (let ((sum (make-connector))
+        (two (make-connector)))
+    (adder m n sum)
+    (multiplier avg two sum)
+    (constant 2 two)
+    'ok))
+
+(test-group "3.33"
+            (define M (make-connector))
+            (define N (make-connector))
+            (define A (make-connector))
+
+            (define (test-average m n a)
+              (forget-value! M 'user)
+              (forget-value! N 'user)
+              (forget-value! A 'user)
+              (set-value! M m 'user)
+              (set-value! N n 'user)
+
+              (test a (get-value A)))
+
+            (averager M N A)
+            (test-average 0 0 0)
+            (test-average 2 0 1.0)
+            (test-average 2 4 3.0))

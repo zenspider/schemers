b4620938d8ca99b6be2b99ffe1cd552b3334a054
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-19 14:33:07 -0800

    3.38

--- sicp/ch_3/exercise.3.38.scm
+++ sicp/ch_3/exercise.3.38.scm
@@ -1,9 +1,4 @@
 
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
-
 ;;; Exercise 3.38
 
 ;; Suppose that Peter, Paul, and Mary share a joint
@@ -19,11 +14,18 @@
 ;;      these three transactions have been completed, assuming that
 ;;      the banking system forces the three processes to run
 ;;      sequentially in some order.
-;;
+
+
+;; a b c = 100 + 10 - 20 - half = 45
+;; a c b = 100 + 10 - half - 20 = 35
+;; b a c = 100 - 20 + 10 - half = 45
+;; b c a = 100 - 20 - half + 10 = 50
+;; c a b = 100 - half + 10 - 20 = 40
+;; c b a = 100 - half - 20 + 10 = 40
+
 ;;   b. What are some other values that could be produced if the
 ;;      system allows the processes to be interleaved?  Draw timing
 ;;      diagrams like the one in *Note Figure 3-29:: to explain how
 ;;      these values can occur.
 
-;; (assert-equal x y)
-(done)
+;; no. tedious.

c7d35327a56562668eb078e55d352c83b3840a4b
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-20 15:33:03 -0800

    3.47

--- sicp/ch_3/exercise.3.47.scm
+++ sicp/ch_3/exercise.3.47.scm
@@ -1,9 +1,3 @@
-
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
-
 ;;; Exercise 3.47
 
 ;; A semaphore (of size n) is a generalization of a
@@ -14,8 +8,25 @@
 ;; implementations of semaphores
 ;;
 ;;   a. in terms of mutexes
-;;
+
+(define (make-semaphore1 max)
+  (let ((lock (make-mutex))
+        (count 0))
+    (define (incr)
+      (if (< count max)
+          (set! count (+ 1 count))
+          (incr)))
+    (define (decr)
+      (set! count (- 1 count)))
+    (define (the-semaphore m)
+      (cond ((eq? m 'acquire)
+             (lock 'acquire)
+             (incr)
+             (lock 'release))
+            ((eq? m 'release)
+             (decr))))))
+
 ;;   b. in terms of atomic `test-and-set!' operations.
 
-;; (assert-equal x y)
-(done)
+;; I could do it... but why bother? The mutex one works fine (hopefully).
+

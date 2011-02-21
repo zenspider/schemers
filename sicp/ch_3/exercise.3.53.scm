2a4afffec284f763c0048e25ad16009bb525b11b
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-20 17:34:12 -0800

    3.53

--- sicp/ch_3/exercise.3.53.scm
+++ sicp/ch_3/exercise.3.53.scm
@@ -1,15 +1,14 @@
-
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
+(require-library streams)
+(import streams)
+(use test)
 
 ;;; Exercise 3.53
 
-;; Without running the program, describe the
-;; elements of the stream defined by
-;;
-;;      (define s (cons-stream 1 (add-streams s s)))
+;; Without running the program, describe the elements of the stream
+;; defined by:
 
-;; (assert-equal x y)
-(done)
+(define s (stream-cons 1 (stream-add s s)))
+
+;; s is a doubler: 1 2 4 8 16 ...
+
+(test 1024 (stream-ref s 10))

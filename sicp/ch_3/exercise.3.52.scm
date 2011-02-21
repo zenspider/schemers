bd27b669202ee4e16ca8b96263cbeb2cfe81050a
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-20 16:41:32 -0800

    3.52

--- sicp/ch_3/exercise.3.52.scm
+++ sicp/ch_3/exercise.3.52.scm
@@ -1,8 +1,6 @@
-
-(require 'testes)
-(import testes)
-(require 'myutils)
-(import myutils)
+(require 'streams)
+(import streams)
+(use test)
 
 ;;; Exercise 3.52
 
@@ -30,5 +28,38 @@
 ;; `(lambda () <EXP>)' without using the optimization provided by
 ;; `memo-proc'?  Explain
 
-;; (assert-equal x y)
-(done)
+
+(define sum 0)
+
+(test "initial value" 0 sum)
+
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+                         seq))
+
+;; seq = 1 3 6 10 15 21 28 36 45 55 66 78 91 105 120 136 153 171 190 210
+;;   y = 6 10 28 36 66 78 120 136 ...
+;;   z = 10 15 45 55 ...
+
+(stream-ref y 7)
+
+(test "(stream-ref y 7)" 136 sum)       ; the 7th even number in seq
+
+(stream-display z)
+
+;; 10
+;; 15
+;; 45
+;; 55
+;; 105
+;; 120
+;; 190
+;; 210
+
+(test "(stream-display z)" 210 sum)     ; the last item in sum
+

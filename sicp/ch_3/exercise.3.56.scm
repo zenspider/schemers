530606a621ff940c2e87bb085a6f134223346170
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-21 21:16:42 -0800

    3.56

--- sicp/ch_3/exercise.3.56.scm
+++ sicp/ch_3/exercise.3.56.scm
@@ -1,4 +1,6 @@
 (use test)
+(require-library streams)
+(import streams)
 
 ;;; Exercise 3.56
 
@@ -13,38 +15,48 @@
 ;; about it.
 ;;
 ;;    * `S' begins with 1.
-;;
 ;;    * The elements of `(scale-stream S 2)' are also elements of `S'.
-;;
-;;    * The same is true for `(scale-stream S 3)' and `(scale-stream
-;;      5 S)'.
-;;
+;;    * The same is true for `(scale-stream S 3)' and `(scale-stream 5 S)'.
 ;;    * These are all the elements of `S'.
-;;
 ;;
 ;; Now all we have to do is combine elements from these sources.  For
 ;; this we define a procedure `merge' that combines two ordered
 ;; streams into one ordered result stream, eliminating repetitions:
-;;
-;;      (define (merge s1 s2)
-;;        (cond ((stream-null? s1) s2)
-;;              ((stream-null? s2) s1)
-;;              (else
-;;               (let ((s1car (stream-car s1))
-;;                     (s2car (stream-car s2)))
-;;                 (cond ((< s1car s2car)
-;;                        (cons-stream s1car (merge (stream-cdr s1) s2)))
-;;                       ((> s1car s2car)
-;;                        (cons-stream s2car (merge s1 (stream-cdr s2))))
-;;                       (else
-;;                        (cons-stream s1car
-;;                                     (merge (stream-cdr s1)
-;;                                            (stream-cdr s2)))))))))
-;;
-;; Then the required stream may be constructed with `merge', as
+
+(define (stream-merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (stream-cons s1car (stream-merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (stream-cons s2car (stream-merge  s1 (stream-cdr s2))))
+                 (else
+                  (stream-cons s1car
+                               (stream-merge (stream-cdr s1)
+                                             (stream-cdr s2)))))))))
+
+;; Then the required stream may be constructed with `stream-merge', as
 ;; follows:
 ;;
-;;      (define S (cons-stream 1 (merge <??> <??>)))
+;;      (define S (stream-cons 1 (stream-merge <??> <??>)))
 ;;
 ;; Fill in the missing expressions in the places marked <??> above.
 
+(define ones     (stream-cons 1 ones))
+(define integers (stream-cons 1 (stream-add ones integers)))
+
+;; TODO: maybe: I think it'd be cleaner to produce: 2^i·3^j·5^k : [ijk] >= 0
+(define S (stream-cons 1 (stream-merge (stream-scale S 2)
+                                       (stream-merge (stream-scale S 3)
+                                                     (stream-scale S 5)))))
+
+;; Show the 1691st Hamming number (the last one below 231).
+
+(test-group "3.56"
+            (test '(1 2 3 4 5 6 8 9 10 12 15 16 18 20
+                      24 25 27 30 32 36 40 45 48 50 54 60)
+                  (stream-head S 26))
+            (test 2125764000 (stream-ref S 1690)))

e868a537a2bb63a7c76535f5596ec5c5ce95b45b
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-21 21:18:08 -0800

    3.57

--- sicp/ch_3/exercise.3.57.scm
+++ sicp/ch_3/exercise.3.57.scm
@@ -2,11 +2,11 @@
 
 ;;; Exercise 3.57
 
-;; How many additions are performed when we compute
-;; the nth Fibonacci number using the definition of `fibs' based on
-;; the `add-streams' procedure?  Show that the number of additions
-;; would be exponentially greater if we had implemented `(delay
-;; <EXP>)' simply as `(lambda () <EXP>)', without using the
-;; optimization provided by the `memo-proc' procedure described in
-;; section *Note 3-5-1::.(5)
+;; How many additions are performed when we compute the nth Fibonacci
+;; number using the definition of `fibs' based on the `add-streams'
+;; procedure? Show that the number of additions would be exponentially
+;; greater if we had implemented `(delay <EXP>)' simply as `(lambda ()
+;; <EXP>)', without using the optimization provided by the `memo-proc'
+;; procedure described in section *Note 3-5-1::.(5)
 
+;; no

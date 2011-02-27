4976a43e757a755c5a131619274a23b1471dea7c
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-27 15:59:31 -0800

    3.68

--- sicp/ch_3/exercise.3.68.scm
+++ sicp/ch_3/exercise.3.68.scm
@@ -1,19 +1,38 @@
 (use test)
+(require-library streams)
+(import streams)
+(use numbers)
 
 ;;; Exercise 3.68
 
-;; Louis Reasoner thinks that building a stream of
-;; pairs from three parts is unnecessarily complicated.  Instead of
-;; separating the pair (S_0,T_0) from the rest of the pairs in the
-;; first row, he proposes to work with the whole first row, as
-;; follows:
-;;
-;;      (define (pairs s t)
-;;        (interleave
-;;         (stream-map (lambda (x) (list (stream-car s) x))
-;;                     t)
-;;         (pairs (stream-cdr s) (stream-cdr t))))
-;;
-;; Does this work?  Consider what happens if we evaluate `(pairs
+;; Louis Reasoner thinks that building a stream of pairs from three
+;; parts is unnecessarily complicated. Instead of separating the pair
+;; (S_0,T_0) from the rest of the pairs in the first row, he proposes
+;; to work with the whole first row, as follows:
+
+(define (interleave s1 s2)
+  (if (stream-null? s1) s2
+      (stream-cons (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (interleave
+   (stream-map (lambda (x) (list (stream-car s) x)) t)
+   (pairs (stream-cdr s) (stream-cdr t))))
+
+#;(define (pairs s t)
+  (stream-cons (list (stream-car s) (stream-car t))
+               (interleave
+                (stream-map (lambda (x) (list (stream-car s) x)) (stream-cdr t))
+                (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (pairs s t)
+  (interleave
+   (stream-map (lambda (x) (list (stream-car s) x)) t)
+   (pairs (stream-cdr s) (stream-cdr t))))
+
+;; Does this work? Consider what happens if we evaluate `(pairs
 ;; integers integers)' using Louis's definition of `pairs'.
 
+;; this causes an infinite loop... but I don't actually know why HELP
+;; (define int-pairs (pairs integers integers))

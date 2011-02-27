e5f7312f42cfa24073cfe296e094098ea0f60195
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-26 16:53:05 -0800

    3.60

--- sicp/ch_3/exercise.3.60.scm
+++ sicp/ch_3/exercise.3.60.scm
@@ -1,4 +1,7 @@
 (use test)
+(use numbers)
+(require-library streams)
+(import streams)
 
 ;;; Exercise 3.60
 
@@ -13,3 +16,35 @@
 ;; You can test your procedure by verifying that sin^2 x + cos^2 x =
 ;; 1, using the series from *Note Exercise 3-59::.
 
+;; (load "ch_3/exercise.3.59.scm")
+
+(define (stream-mul s1 s2)
+  (stream-map * s1 s2))
+
+(define nths (stream-map (lambda (x) (/ 1 x)) integers))
+(define (integrate-series A) (stream-mul nths A))
+
+(define cosine-series
+  (stream-cons 1 (integrate-series (stream-map - sine-series))))
+
+(define sine-series
+  (stream-cons 0 (integrate-series cosine-series)))
+
+(define (mul-series s1 s2)
+  (stream-cons (* (stream-car s1) (stream-car s2))
+               (stream-add (stream-scale (stream-cdr s2) (stream-car s1))
+                            (mul-series (stream-cdr s1) s2))))
+
+(define cos2-series      (mul-series cosine-series cosine-series))
+(define sin2-series      (mul-series   sine-series   sine-series))
+(define cos2+sin2-series (stream-add   sin2-series   cos2-series))
+
+;; (stream-head cosine-series 10)
+;; (stream-head sine-series 10)
+;; (stream-head cos2-series 10)
+;; (stream-head sin2-series 10)
+
+#;(test-group "3.60"
+            (test 1      (stream-ref  cos2+sin2-series 0))
+            (test 0      (stream-ref  cos2+sin2-series 1))
+            (test '(1 0) (stream-head cos2+sin2-series 2)))

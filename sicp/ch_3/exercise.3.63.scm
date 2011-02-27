eee16fb92bb6a8eca49bad84a8ea6092dcc35fc3
Ryan Davis <ryand-ruby@zenspider.com>
2011-02-26 16:52:41 -0800

    3.63

--- sicp/ch_3/exercise.3.63.scm
+++ sicp/ch_3/exercise.3.63.scm
@@ -2,9 +2,9 @@
 
 ;;; Exercise 3.63
 
-;; Louis Reasoner asks why the `sqrt-stream'
-;; procedure was not written in the following more straightforward
-;; way, without the local variable `guesses':
+;; Louis Reasoner asks why the `sqrt-stream' procedure was not written
+;; in the following more straightforward way, without the local
+;; variable `guesses':
 ;;
 ;;      (define (sqrt-stream x)
 ;;        (stream-cons 1.0
@@ -14,8 +14,13 @@
 ;;
 ;; Alyssa P. Hacker replies that this version of the procedure is
 ;; considerably less efficient because it performs redundant
-;; computation.  Explain Alyssa's answer.  Would the two versions
-;; still differ in efficiency if our implementation of `delay' used
-;; only `(lambda () <EXP>)' without using the optimization provided
-;; by `memo-proc' (section *Note 3-5-1::)?
+;; computation. Explain Alyssa's answer. Would the two versions still
+;; differ in efficiency if our implementation of `delay' used only
+;; `(lambda () <EXP>)' without using the optimization provided by
+;; `memo-proc' (section *Note 3-5-1::)?
 
+;; A: Alyssa is correct. By using tail-recursion, each stream-cdr is
+;; forking to a new stream, rather than consing to itself.
+;;
+;; memoization doesn't help or hinder this issue, at least in terms of
+;; space.

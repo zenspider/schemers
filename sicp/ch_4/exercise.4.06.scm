960cd251735d6e9d614fab2a5e2f0b4c2ae390bd
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-12 22:07:01 -0800

    4.06

--- sicp/ch_4/exercise.4.06.scm
+++ sicp/ch_4/exercise.4.06.scm
@@ -21,3 +21,32 @@
 ;; reduces evaluating `let' expressions to evaluating combinations of
 ;; the type shown above, and add the appropriate clause to `eval' to
 ;; handle `let' expressions.
+
+(define (let-args exp)
+  (map car (cadr exp)))
+
+(define (let-vals exp)
+  (map cadr (cadr exp)))
+
+(define let-body cddr)
+
+(define (let->call args vals body)
+  (append (list (append (list 'lambda args) body)) vals))
+
+(define (let->combination exp)
+  (let->call (let-args exp) (let-vals exp) (let-body exp)))
+
+(test-group "4.06"
+            (define mylet '(let ((a 1)
+                                 (b 2)
+                                 (c 3))
+                             body))
+
+                        (test '(a b c) (let-args mylet))
+                        (test '(1 2 3) (let-vals mylet))
+                        (test '(body)  (let-body mylet))
+
+            (test '((lambda (a b c) body) 1 2 3)
+                  (let->combination mylet)))
+
+

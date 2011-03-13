f665bd9f3aca1d0d8df5430ad51b116de7cec185
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-12 21:35:17 -0800

    4.03 + supporting exports to eval.scm

--- sicp/ch_4/exercise.4.03.scm
+++ sicp/ch_4/exercise.4.03.scm
@@ -4,8 +4,36 @@
 
 ;;; Exercise 4.3
 
-;; Rewrite `eval' so that the dispatch is done in
-;; data-directed style.  Compare this with the data-directed
-;; differentiation procedure of *Note Exercise 2-73::.  (You may use
-;; the `car' of a compound expression as the type of the expression,
-;; as is appropriate for the syntax implemented in this section.)
+;; Rewrite `eval' so that the dispatch is done in data-directed style.
+;; Compare this with the data-directed differentiation procedure of
+;; *Note Exercise 2-73::. (You may use the `car' of a compound
+;; expression as the type of the expression, as is appropriate for the
+;; syntax implemented in this section.)
+
+(require-library eval)
+(import eval)
+
+(define defines '())
+
+(define (def-set name lambda)
+  (set! defines (append defines (list (list name lambda)))))
+
+(define (def-get name)
+  (cdr (or (assoc name defines) (assoc 'apply defines))))
+
+(define (dd-eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable?        exp) (lookup-variable-value exp env))
+        (else
+         ((def-get (operator exp)) (operands exp) env))))
+
+(def-set 'quote  (lambda (exp env) (text-of-quotation exp)))
+(def-set 'set!   (lambda (exp env) (eval-assignment exp env)))
+(def-set 'define (lambda (exp env) (eval-definition exp env)))
+(def-set 'if     (lambda (exp env) (eval-if exp env)))
+(def-set 'lambda (lambda (exp env) (make-procedure (lambda-parameters exp)
+                                                   (lambda-body exp) env)))
+(def-set 'begin  (lambda (exp env) (eval-sequence (begin-actions exp) env)))
+(def-set 'cond   (lambda (exp env) (eval (cond->if exp) env)))
+(def-set 'apply  (lambda (exp env) (apply (eval (operator exp) env)
+                                          (list-of-values (operands exp) env))))

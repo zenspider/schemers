29ec52237ba20cc99a45eb86b4208033239fa298
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-19 19:56:46 -0700

    4.14

--- sicp/ch_4/exercise.4.14.scm
+++ sicp/ch_4/exercise.4.14.scm
@@ -6,10 +6,15 @@
 
 ;;; Exercise 4.14
 
-;; Eva Lu Ator and Louis Reasoner are each
-;; experimenting with the metacircular evaluator.  Eva types in the
-;; definition of `map', and runs some test programs that use it.
-;; They work fine.  Louis, in contrast, has installed the system
-;; version of `map' as a primitive for the metacircular evaluator.
-;; When he tries it, things go terribly wrong.  Explain why Louis's
-;; `map' fails even though Eva's works.
+;; Eva Lu Ator and Louis Reasoner are each experimenting with the
+;; metacircular evaluator. Eva types in the definition of `map', and
+;; runs some test programs that use it. They work fine. Louis, in
+;; contrast, has installed the system version of `map' as a primitive
+;; for the metacircular evaluator. When he tries it, things go
+;; terribly wrong. Explain why Louis's `map' fails even though Eva's
+;; works.
+
+;; A: make-define creates a list to be used as a proc in the
+;;    evaluator, not a real scheme proc. Installing the scheme map
+;;    means that the evaluator needs to produce real scheme procs, not
+;;    internalized versions of procs.

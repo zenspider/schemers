c533a7df369c1df59d8621ca1b0afe9027bab4e1
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-19 19:16:03 -0700

    4.12

--- sicp/ch_4/exercise.4.12.scm
+++ sicp/ch_4/exercise.4.12.scm
@@ -6,9 +6,10 @@
 
 ;;; Exercise 4.12
 
-;; The procedures `set-variable-value!',
-;; `define-variable!', and `lookup-variable-value' can be expressed
-;; in terms of more abstract procedures for traversing the
-;; environment structure.  Define abstractions that capture the
-;; common patterns and redefine the three procedures in terms of these
-;; abstractions.
+;; The procedures set-variable-value!, define-variable!, and
+;; lookup-variable-value can be expressed in terms of more abstract
+;; procedures for traversing the environment structure. Define
+;; abstractions that capture the common patterns and redefine the
+;; three procedures in terms of these abstractions.
+
+;; STUPID. see 4.11

79542add43e1b5a671cc63477ffab81692827467
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-20 20:56:49 -0700

    4.17

--- sicp/ch_4/exercise.4.17.scm
+++ sicp/ch_4/exercise.4.17.scm
@@ -16,3 +16,37 @@
 ;; of a correct program.  Design a way to make the interpreter
 ;; implement the "simultaneous" scope rule for internal definitions
 ;; without constructing the extra frame.
+
+;; A: there's an extra frame because of the extend-environment in
+;; apply on compound-procedure.
+;;
+;; This won't make a difference in a correct program because the
+;; transformation is algebraically equivalent to the pre-transformed
+;; code. Adding any number of frames won't make a difference as long
+;; as the union of the frames is always the same.
+;;
+;; One way to get rid of the extra scope is to get rid of the
+;; let->lambda phase. Expose define-variable so that set! will create
+;; the var in the current scope, then you can skip the let phase
+;; entirely:
+
+;; (lambda (...)
+;;   (letrec ((a (lambda () ...))
+;;            (b (lambda () ...)))
+;;     (f a b)))
+
+;; becomes:
+
+;; (lambda (...)
+;;   (define-var 'a 'b)
+;;   (set! a (lambda () ...))
+;;   (set! b (lambda () ...))
+;;   (f a b))
+
+;; instead of:
+
+;; (lambda (...)
+;;   ((lambda (a b)
+;;      (set! a (lambda () ...))
+;;      (set! b (lambda () ...))
+;;      (f a b)) 42 42))

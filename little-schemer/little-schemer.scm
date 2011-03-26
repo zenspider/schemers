fbefac0a6f4f6e6d56fac7a7b97d44af800a32ce
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-26 15:14:15 -0700

    Did pages 138-156

--- little-schemer/little-schemer.scm
+++ little-schemer/little-schemer.scm
@@ -1161,3 +1161,165 @@
            (multirember&co a (cdr lat)
                            (lambda (newlat seen)
                              (col (cons (car lat) newlat) seen)))))))
+
+;; pg 138
+
+(define a-friend (lambda (x y) (null? y)))
+
+(test #t (multirember&co 'tuna '()                                a-friend))
+(test #f (multirember&co 'tuna '(tuna)                            a-friend))
+(test #f (multirember&co 'tuna '(strawberries tuna and swordfish) a-friend))
+
+;; pg 141
+
+(define multiinsertLR
+  (lambda (new oldL oldR lat)
+    (cond ((null? lat) '())
+          ((eq? (car lat) oldL)
+           (cons new (cons oldL (multiinsertLR new oldL oldR (cdr lat)))))
+          ((eq? (car lat) oldR)
+           (cons oldR (cons new (multiinsertLR new oldL oldR (cdr lat)))))
+          (else (cons (car lat) (multiinsertLR new oldL oldR (cdr lat)))))))
+
+(define multiinsertLR&co
+  (lambda (new oldL oldR lat col)
+    (cond ((null? lat) '())
+          ((eq? (car lat) oldL)
+           (cons new
+                 (cons oldL
+                       (multiinsertLR&co new oldL oldR (cdr lat)
+                                         (lambda (newlat L R)
+                                           (col (cons new (cons oldL newlat))
+                                                (add1 L) R))))))
+          ((eq? (car lat) oldR)
+           (cons oldR
+                 (cons new
+                       (multiinsertLR&co new oldL oldR (cdr lat)
+                                         (lambda (newlat L R)
+                                           (col (cons oldR (cons new newlat))
+                                                L (add1 R)))))))
+          (else (cons (car lat)
+                      (multiinsertLR&co new oldL oldR (cdr lat)
+                                        (lambda (newlat L R)
+                                          (col (cons (car lat) newlat)
+                                               L R))))))))
+
+;; I don't get where this is going at all... I'm gonna try to go
+;; faster to get to the next section.
+
+;; pg 144
+
+(define even?
+  (lambda (n)
+    (= (remainder n 2) 0)))
+
+(test #f (even? 3))
+(test #t (even? 4))
+
+(define evens-only*
+  (lambda (l)
+    (cond ((null? l) '())
+          ((atom? (car l))
+           (cond ((even? (car l)) (cons (car l) (evens-only* (cdr l))))
+                 (else (evens-only* (cdr l)))))
+          (else (cons (evens-only* (car l))
+                      (evens-only* (cdr l)))))))
+
+(test '((2 8) 10 (() 6) 2) (evens-only* '((9 1 2 8) 3 10 ((9 9) 7 6) 2)))
+
+;; fuck it... moving on to the next section.
+
+(define keep-looking
+  (lambda (a sorn lat)
+    (cond ((number? sorn)
+           (keep-looking a (pick sorn lat) lat))
+          (else (eq? sorn a)))))
+
+(define looking
+  (lambda (a lat)
+    (keep-looking a (pick 1 lat) lat)))
+
+(test #t (looking 'caviar '(6 2 4 caviar 5 7 3)))
+(test #f (looking 'caviar '(6 2 grits caviar 5 7 3)))
+
+;; pg 151
+
+(define eternity
+  (lambda (x) (eternity x)))
+
+;; pg 152
+
+(define shift
+  (lambda (pair)
+    (build (first (first pair))
+           (build (second (first pair))
+                  (second pair)))))
+
+(test '(a (b c)) (shift '((a b) c)))
+(test '(a (b (c d))) (shift '((a b) (c d))))
+
+(define align
+  (lambda (pora)
+    (cond ((atom? pora) pora)
+          ((a-pair? (first pora))
+           (align (shift pora)))
+          (else (build (first pora)
+                       (align (second pora)))))))
+
+(define length*
+  (lambda (pora)
+    (cond ((atom? pora) 1)
+          (else (+ (length* (first pora))
+                   (length* (second pora)))))))
+
+(test 2 (length* '(1 2)))
+(test 3 (length* '(1 (2 3))))
+(test 4 (length* '((1 2) (3 4))))
+(test 4 (length* '((1 2 3) (4 5 6))))   ; seems useless
+
+;; pg 154
+
+(define weight*
+  (lambda (pora)
+    (cond ((atom? pora) 1)
+          (else (+ (* (weight* (first pora)) 2)
+                   (weight* (second pora)))))))
+
+(test 7 (weight* '((a b) c)))
+(test 5 (weight* '(a (b c))))
+
+(define shuffle
+  (lambda (pora)
+    (cond ((atom? pora) pora)
+          ((a-pair? (first pora))
+           (shuffle (revpair pora)))
+          (else (build (first pora)
+                       (shuffle (second pora)))))))
+
+(test '(a (b c)) (shuffle '(a (b c))))
+(test '(a b)     (shuffle '(a b)))
+(test '(b a)     (revpair '(a b)))
+
+(define C
+  (lambda (n)
+    (cond ((= 1 n) 1)
+          (else (cond ((even? n) (C (div n 2)))
+                      (else (C (add1 (* 3 n)))))))))
+
+(test 1 (C 1))
+(test 1 (C 2))
+(test 1 (C 3))
+(test 1 (C 4))
+
+;; pg 156
+
+(define A
+  (lambda (n m)
+    (cond ((zero? n) (add1 m))
+          ((zero? m) (A (sub1 n) 1))
+          (else (A (sub1 n) (A n (sub1 m)))))))
+
+(test 2 (A 1 0))
+(test 3 (A 1 1))
+(test 7 (A 2 2))
+

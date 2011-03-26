7f9a999e01841efecade7e7cd41b82f759c4c63f
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-26 16:23:29 -0700

    4.21

--- sicp/ch_4/exercise.4.21.scm
+++ sicp/ch_4/exercise.4.21.scm
@@ -40,23 +40,53 @@
 
 ;; (holy shit!)
 
+(define (fib n)
+  (if (< n 2) n
+      (+ (fib (- n 2)) (fib (- n 1)))))
 
+(test 55 (fib 10))
 
-;;
+(define (Y outer)                       ; derived via little schemer
+  ((lambda (f) (f f))
+   (lambda (f) (outer (lambda (x) ((f f) x))))))
+
+(test 55 ((Y (lambda (fib)              ; clean happy version
+               (lambda (n)
+                 (if (< n 2) n
+                     (+ (fib (- n 2)) (fib (- n 1))))))) 10))
+
+(test 55 (((lambda (outer)              ; subst Y
+             ((lambda (f) (f f))
+              (lambda (f) (outer (lambda (x) ((f f) x))))))
+           (lambda (fib)
+             (lambda (n)
+               (if (< n 2) n
+                   (+ (fib (- n 2)) (fib (- n 1))))))) 10))
+
+(test 55 ((lambda (n)                   ; apply outer - or just copy/mod fact
+            ((lambda (f) (f f n))
+             (lambda (f k)
+               (if (< k 2) k
+                   (+ (f f (- k 2)) (f f (- k 1)))))))
+          10))
+
 ;;   b. Consider the following procedure, which includes mutually
 ;;      recursive internal definitions:
-;;
-;;           (define (f x)
-;;             (define (even? n)
-;;               (if (= n 0)
-;;                   true
-;;                   (odd? (- n 1))))
-;;             (define (odd? n)
-;;               (if (= n 0)
-;;                   false
-;;                   (even? (- n 1))))
-;;             (even? x))
-;;
+
+(define (f x)
+  (define (even? n)
+    (if (= n 0) true
+        (odd? (- n 1))))
+  (define (odd? n)
+    (if (= n 0) false
+        (even? (- n 1))))
+  (even? x))
+
+(test #f (f  1))
+(test #t (f  2))
+(test #f (f 21))
+(test #t (f 22))
+
 ;;      Fill in the missing expressions to complete an alternative
 ;;      definition of `f', which uses neither internal definitions
 ;;      nor `letrec':
@@ -68,3 +98,19 @@
 ;;                (if (= n 0) true (od? <??> <??> <??>)))
 ;;              (lambda (ev? od? n)
 ;;                (if (= n 0) false (ev? <??> <??> <??>)))))
+
+
+(define (f2 x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) true
+         (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) false
+         (ev? ev? od? (- n 1))))))
+
+(test #f (f2  1))
+(test #t (f2  2))
+(test #f (f2 21))
+(test #t (f2 22))

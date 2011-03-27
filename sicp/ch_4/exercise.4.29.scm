ce31ea6017e3e9779aeb7bed9fbd39cd23e43c09
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-26 19:08:14 -0700

    4.29

--- sicp/ch_4/exercise.4.29.scm
+++ sicp/ch_4/exercise.4.29.scm
@@ -1,13 +1,15 @@
 #!/usr/bin/env csi -s
 
 (use test)
+(require-library lazy-eval)
+(import lazy-eval)
 
 ;;; Exercise 4.29
 
-;; Exhibit a program that you would expect to run
-;; much more slowly without memoization than with memoization.  Also,
-;; consider the following interaction, where the `id' procedure is
-;; defined as in *Note Exercise 4-27:: and `count' starts at 0:
+;; Exhibit a program that you would expect to run much more slowly
+;; without memoization than with memoization. Also, consider the
+;; following interaction, where the `id' procedure is defined as in
+;; *Note Exercise 4-27:: and `count' starts at 0:
 ;;
 ;;      (define (square x)
 ;;        (* x x))
@@ -24,3 +26,20 @@
 ;;
 ;; Give the responses both when the evaluator memoizes and when it
 ;; does not.
+
+(define env (setup-environment))
+(test 'ok (eval '(define count 0) env))
+(test 'ok (eval '(define (id x) (set count (+ count 1)) x) env))
+(test 'ok (eval '(define (square x) (* x x)) env))
+(test 'ok (eval '(define s10 (square (id 10))) env))
+(test   1 (eval 'count            env))
+(test 100 (eval 's10              env))
+(test   1 (eval 'count            env))
+(test 100 (eval 's10              env))
+(test   1 (eval 'count            env))
+(test 100 (eval '(square (id 10)) env))
+(test   2 (eval 'count            env))
+(test 100 (eval '(square (id 10)) env))
+(test   3 (eval 'count            env))
+
+;; idgi

9a1b5ffb2a51e840141e18761c36f80e55c1b3fc
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-26 19:08:12 -0700

    4.28

--- sicp/ch_4/exercise.4.28.scm
+++ sicp/ch_4/exercise.4.28.scm
@@ -8,3 +8,9 @@
 ;; evaluate the operator before passing it to `apply', in order to
 ;; force the value of the operator.  Give an example that
 ;; demonstrates the need for this forcing.
+
+;; wouldn't something as simple as this count?
+
+(define call (lambda (f x) (f x)))
+(define add1 (lambda (n) (+ n 1)))
+(call add1 2)

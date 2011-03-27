8441805fd82dc3cc140362356070675fa8cf3a17
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-26 17:38:06 -0700

    4.24

--- sicp/ch_4/exercise.4.24.scm
+++ sicp/ch_4/exercise.4.24.scm
@@ -10,3 +10,5 @@
 ;; the speed of the original metacircular evaluator with the version
 ;; in this section.  Use your results to estimate the fraction of time
 ;; that is spent in analysis versus execution for various procedures.
+
+;; no

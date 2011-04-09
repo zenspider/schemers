f0bc54e6559c1fb77c60f9cd6b09e6b912f24b27
Ryan Davis <ryand-ruby@zenspider.com>
2011-04-08 19:13:21 -0700

    Switched to backwards comparison. Improved tests.

--- little-schemer/ch11.scm
+++ little-schemer/ch11.scm
@@ -2,20 +2,22 @@
 
 (use test)
 
-(define is-first-b?
-  (lambda (a lat)
+(define two-in-a-row-b?
+  (lambda (preceding lat)
     (cond ((null? lat) #f)
-          (else (or (eq? (car lat) a)
-                    (two-in-a-row? lat))))))
+          (else (or (eq? (car lat) preceding)
+                    (two-in-a-row-b? (car lat) (cdr lat)))))))
 
 (define two-in-a-row?
   (lambda (lat)
     (cond ((null? lat) #f)
           (else
-           (is-first-b? (car lat) (cdr lat))))))
+           (two-in-a-row-b? (car lat) (cdr lat))))))
 
 (test-group "two-in-a-row?"
- (test #f (two-in-a-row? '(a b c)))
- (test #f (two-in-a-row? '(a b a)))
- (test #t (two-in-a-row? '(a b b))))
+ (test #f (two-in-a-row? '(a b c d)))
+ (test #f (two-in-a-row? '(a b a d)))
+ (test #t (two-in-a-row? '(a b b d)))
+ (test #t (two-in-a-row? '(a a c d)))
+ (test #t (two-in-a-row? '(a b c c))))
 

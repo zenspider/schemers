8bcc14e764e8a56116a2923ddc6d96f9a3b6e539
Ryan Davis <ryand-ruby@zenspider.com>
2011-04-09 19:28:19 -0700

    sum-of-prefixes and scramble

--- little-schemer/ch11.scm
+++ little-schemer/ch11.scm
@@ -21,3 +21,40 @@
  (test #t (two-in-a-row? '(a a c d)))
  (test #t (two-in-a-row? '(a b c c))))
 
+(define sum-of-prefixes-b
+  (lambda (sum tup)
+    (cond ((null? tup) '())
+          (else (cons (+ sum (car tup))
+                 (sum-of-prefixes-b (+ sum (car tup)) (cdr tup)))))))
+
+(define sum-of-prefixes
+  (lambda (tup)
+    (sum-of-prefixes-b 0 tup)))
+
+(test-group "sum-of-prefixes"
+  (test '(1 2 3 4 5)     (sum-of-prefixes '(1 1 1 1 1)))
+  (test '(2 3 12 29 29)  (sum-of-prefixes '(2 1 9 17 0))))
+
+;; 11th commandment: Use additional arguments when a function needs to
+;;                   know what other arguments to the function have
+;;                   been so far.
+
+(define pick
+  (lambda (n lat)
+    (cond ((= 1 n) (car lat))
+          (else (pick (sub1 n) (cdr lat))))))
+
+(define scramble-b
+  (lambda (tup rev-pre)
+    (cond ((null? tup) '())
+          (else
+           (cons (pick (car tup) (cons (car tup) rev-pre))
+                 (scramble-b (cdr tup) (cons (car tup) rev-pre)))))))
+
+(define scramble
+  (lambda (tup)
+    (scramble-b tup '())))
+
+(test-group "scramble"
+  (test '(1 1 1 1 1 4 1 1 1 9) (scramble '(1 1 1 3 4 2 1 1 9 2)))
+  (test '(1 1 1 1 1 1 1 1 1)   (scramble '(1 2 3 4 5 6 7 8 9))))

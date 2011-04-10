b9acd9bacac38af38c4bc1a61e0b9165781efbdc
Ryan Davis <ryand-ruby@zenspider.com>
2011-04-10 15:10:47 -0700

    4.54

--- sicp/ch_4/exercise.4.54.scm
+++ sicp/ch_4/exercise.4.54.scm
@@ -31,3 +31,14 @@
 ;;                         <??>
 ;;                         (succeed 'ok fail2)))
 ;;                   fail))))
+
+
+(define (analyze-require exp)
+  (let ((pproc (analyze (require-predicate exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             (lambda (pred-value fail2)
+               (if (not pred-value)     ; why write it negative first?
+                   (fail)
+                   (succeed 'ok fail2)))
+             fail))))

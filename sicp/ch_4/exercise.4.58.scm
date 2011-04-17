e9aea479a37f80b73f7c60af6089e38932426ede
Ryan Davis <ryand-ruby@zenspider.com>
2011-04-16 19:32:09 -0700

    Added some refinements with some help from Eli

--- sicp/ch_4/exercise.4.58.scm
+++ sicp/ch_4/exercise.4.58.scm
@@ -11,17 +11,26 @@
 ;; if the person works in the division but does not have a supervisor
 ;; who works in the division.
 
-(pp (all-of '(and (job ?p1 (?div1 . ?iggy1))
-                  (supervisor ?p1 ?p2)
-                  (job ?p2 (?div2 . ?iggy2))
-                  (not (same ?div1 ?div2)))))
-
 (assert! '(rule (bigshot ?p1 ?p2)
                 (and (job ?p1 (?div1 . ?iggy1))
-                     (supervisor ?p1 ?p2)
-                     (job ?p2 (?div2 . ?iggy2))
-                     (not (same ?div1 ?div2)))))
+                     (or (not (supervisor ?p1 ?p2))
+                         (and (supervisor ?p1 ?p2)
+                              (not (job ?p2 (?div1 . ?iggy2))))))))
 
-(test '((bigshot (Scrooge Eben)  (Warbucks Oliver))
+(test '((bigshot (Warbucks Oliver) ?p2-1)
+        (bigshot (Scrooge Eben)  (Warbucks Oliver))
         (bigshot (Bitdiddle Ben) (Warbucks Oliver)))
       (all-of '(bigshot ?p1 ?p2)))
+
+;; found on http://eli.thegreenplace.net/2008/02/08/sicp-section-441/
+(assert! '(rule (bigshot2 ?p1 ?div)
+                (and (job ?p1 (?div . ?iggy1))
+                     (or (not (supervisor ?p1 ?p2))
+                         (and (supervisor ?p1 ?p2)
+                              (not (job ?p2 (?div . ?iggy2))))))))
+
+(test '((bigshot2 (Warbucks Oliver) administration)
+        (bigshot2 (Scrooge Eben) accounting)
+        (bigshot2 (Bitdiddle Ben) computer))
+      (all-of '(bigshot2 ?p1 ?p2)))
+

a1348bb45a4016bd960f4ec163289735299c868f
Ryan Davis <ryand-ruby@zenspider.com>
2011-04-16 19:27:40 -0700

    4.58

--- sicp/ch_4/exercise.4.58.scm
+++ sicp/ch_4/exercise.4.58.scm
@@ -1,9 +1,27 @@
 #!/usr/bin/env csi -s
 
 (use test)
+(require-library logic-eval)
+(import logic-eval)
+(initialize-data-base microshaft-data-base)
 
 ;;; Exercise 4.58
 
-;; Define a rule that says that a person is a "big
-;; shot" in a division if the person works in the division but does
-;; not have a supervisor who works in the division.
+;; Define a rule that says that a person is a "big shot" in a division
+;; if the person works in the division but does not have a supervisor
+;; who works in the division.
+
+(pp (all-of '(and (job ?p1 (?div1 . ?iggy1))
+                  (supervisor ?p1 ?p2)
+                  (job ?p2 (?div2 . ?iggy2))
+                  (not (same ?div1 ?div2)))))
+
+(assert! '(rule (bigshot ?p1 ?p2)
+                (and (job ?p1 (?div1 . ?iggy1))
+                     (supervisor ?p1 ?p2)
+                     (job ?p2 (?div2 . ?iggy2))
+                     (not (same ?div1 ?div2)))))
+
+(test '((bigshot (Scrooge Eben)  (Warbucks Oliver))
+        (bigshot (Bitdiddle Ben) (Warbucks Oliver)))
+      (all-of '(bigshot ?p1 ?p2)))

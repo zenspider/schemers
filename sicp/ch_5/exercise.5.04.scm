11ccca8f039c3117ac90a46ff85262b0f064cd93
Ryan Davis <ryand-ruby@zenspider.com>
2011-05-07 16:57:52 -0700

    5.04

--- sicp/ch_5/exercise.5.04.scm
+++ sicp/ch_5/exercise.5.04.scm
@@ -1,6 +1,7 @@
 #!/usr/bin/env csi -s
 
 (use test)
+(load "../lib/ch5-regsim.scm")
 
 ;;; Exercise 5.4
 
@@ -23,3 +24,55 @@
 ;;                   product
 ;;                   (expt-iter (- counter 1) (* b product))))
 ;;             (expt-iter n 1))
+
+(define expt-recursive
+  (make-machine
+   '(b n product continue)
+   (list (list '= =)
+         (list '* *)
+         (list '- -))
+   '(controller
+       (assign continue (label expt-done))
+     expt-loop
+       (test (op =) (reg n) (const 0))
+       (branch (label base-case))
+       ;; store and loop
+       (save continue)
+       (save n)
+       (assign n (op -) (reg n) (const 1))
+       (assign continue (label after-expt))
+       (goto (label expt-loop))
+     after-expt
+       ;; restore and unwind
+       (restore n)
+       (restore continue)
+       (assign product (op *) (reg b) (reg product))
+       (goto (reg continue))
+     base-case
+       ;; bottom out at 1
+       (assign product (const 1))
+       (goto (reg continue))
+     expt-done)))
+
+(define expt-iterative
+  (make-machine
+   '(b n counter product)
+   (list (list '= =)
+         (list '* *)
+         (list '- -))
+   '(controller
+       (assign counter (reg n))
+       (assign product (const 1))
+     expt
+       (test           (op =) (reg counter) (const 0))
+       (branch (label expt-done))
+
+       (assign counter (op -) (reg counter) (const 1))
+       (assign product (op *) (reg product) (reg b))
+
+       (goto (label expt))
+     expt-done
+     )))
+
+(assert-machine expt-recursive '((b 2) (n 3)) 'product 8)
+(assert-machine expt-iterative '((b 2) (n 3)) 'product 8)

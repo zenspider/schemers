525a2e7abb283965d369cca24a861540ced8734f
Ryan Davis <ryand-ruby@zenspider.com>
2011-05-07 15:01:56 -0700

    5.02

--- sicp/ch_5/exercise.5.02.scm
+++ sicp/ch_5/exercise.5.02.scm
@@ -1,8 +1,20 @@
 #!/usr/bin/env csi -s
 
-(use test)
-
 ;;; Exercise 5.2
 
-;; Use the register-machine language to describe the
-;; iterative factorial machine of *Note Exercise 5-1::.
+;; Use the register-machine language to describe the iterative
+;; factorial machine of *Note Exercise 5-1::.
+
+;; (controller
+;;  factorial
+;;  (assign product (const 1))
+;;  (assign counter (const 1))
+;;  factorial-top
+;;  (test (op >) (reg counter) (reg n))
+;;  (branch (label factorial-done))
+;;  (assign t (op *) (reg counter) (reg product))
+;;  (assign product (reg t))
+;;  (assign t (op +) (reg counter) (contst 1))
+;;  (assign counter (reg t))
+;;  (goto (label factorial-top))
+;;  factorial-done)

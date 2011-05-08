62967e2e8141c3f49b4c026f95074755d635e0c4
Ryan Davis <ryand-ruby@zenspider.com>
2011-05-07 17:10:13 -0700

    ugh: 5.06

--- sicp/ch_5/exercise.5.06.scm
+++ sicp/ch_5/exercise.5.06.scm
@@ -4,7 +4,15 @@
 
 ;;; Exercise 5.6
 
-;; Ben Bitdiddle observes that the Fibonacci
-;; machine's controller sequence has an extra `save' and an extra
-;; `restore', which can be removed to make a faster machine.  Where
-;; are these instructions?
+;; Ben Bitdiddle observes that the Fibonacci machine's controller
+;; sequence has an extra `save' and an extra `restore', which can be
+;; removed to make a faster machine. Where are these instructions?
+
+;;  afterfib-n-1
+;;    (restore n)
+;;    (restore continue) ;; not necessary as we don't jump in-between
+;;    (assign n (op -) (reg n) (const 2))
+;;    (save continue)    ;; not necessary as we don't jump in-between
+;;    (assign continue (label afterfib-n-2))
+;;    (save val)
+;;    (goto (label fib-loop))

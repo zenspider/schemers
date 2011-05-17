111873cb07aac4f665647b871d4aa04114ac8433
Ryan Davis <ryand-ruby@zenspider.com>
2011-05-16 20:22:37 -0700

    whatever

--- sicp/ch_5/exercise.5.21.scm
+++ sicp/ch_5/exercise.5.21.scm
@@ -1,6 +1,8 @@
 #!/usr/bin/env csi -s
 
 (use test)
+(require-library machine)
+(import machine)
 
 ;;; Exercise 5.21
 
@@ -9,19 +11,90 @@
 ;; available as machine primitives.
 ;;
 ;;   a. Recursive `count-leaves':
-;;
-;;           (define (count-leaves tree)
-;;             (cond ((null? tree) 0)
-;;                   ((not (pair? tree)) 1)
-;;                   (else (+ (count-leaves (car tree))
-;;                            (count-leaves (cdr tree))))))
-;;
+
+(define (count-leaves-r tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree)) 1)
+        (else (+ (count-leaves-r (car tree))
+                 (count-leaves-r (cdr tree))))))
+
 ;;   b. Recursive `count-leaves' with explicit counter:
+
+(define (count-leaves-c tree)
+  (define (count-iter tree n)
+    (cond ((null? tree) n)
+          ((not (pair? tree)) (+ n 1))
+          (else (count-iter (cdr tree)
+                            (count-iter (car tree) n)))))
+  (count-iter tree 0))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define x (cons 1 2))
+(define y (list x x))
+
+(test 4 (count-leaves-r y))
+(test 4 (count-leaves-c y))
+
+;; (define count-leaves-c-m
+;;   (make-machine
+;;    '(tree n t)
+;;    (list xxx)
+;;    '(controller
+;;      ;; (define (count-leaves-c tree)
+;;      ;;   (define (count-iter tree n)
+;;      ;;     (cond ((null? tree) n)
+;;      ;;           ((not (pair? tree)) (+ n 1))
+;;      ;;           (else (count-iter (cdr tree)
+;;      ;;                             (count-iter (car tree) n)))))
+;;      ;;   (count-iter tree 0))
 ;;
-;;           (define (count-leaves tree)
-;;             (define (count-iter tree n)
-;;               (cond ((null? tree) n)
-;;                     ((not (pair? tree)) (+ n 1))
-;;                     (else (count-iter (cdr tree)
-;;                                       (count-iter (car tree) n)))))
-;;             (count-iter tree 0))
+;;      (assign n (const 0))
+;;
+;;      count-loop
+;;
+;;      (test (op null?) tree)
+;;      (branch (label done))
+;;
+;;      (test (op pair?) tree)
+;;      (branch (label recurse))
+;;
+;;      recurse
+;;
+;;      done
+;; )))
+
+;; (test 4 (count-leaves-r-m y))
+;; (test 4 (count-leaves-c-m y))
+
+;; (define factorial
+;;   (make-machine
+;;    '(n continue product)
+;;    (list (list '= =) (list '- -) (list '* *))
+;;    '(controller
+;;        (assign continue (label fact-done))
+;;      fact-loop
+;;        (test (op =) (reg n) (const 1))
+;;        (branch (label base-case))
+;;        (save continue)
+;;        (save n)
+;;        (assign n (op -) (reg n) (const 1))
+;;        (assign continue (label after-fact))
+;;        (goto (label fact-loop))
+;;      after-fact
+;;        (restore n)
+;;        (restore continue)
+;;        (assign product (op *) (reg n) (reg product))
+;;        (goto (reg continue))
+;;      base-case
+;;        (assign product (const 1))
+;;        (goto (reg continue))
+;;      fact-done)))
+;;
+;; (assert-machine factorial '((n 1)) 'product 1)
+;; (assert-machine factorial '((n 2)) 'product 2)
+;; (assert-machine factorial '((n 3)) 'product 6)
+;; (assert-machine factorial '((n 4)) 'product 24)
+;; (trace-register-on factorial 'product)
+;; (assert-machine factorial '((n 5)) 'product 120)
+;; (trace-register-off factorial 'product)
--- sicp/ch_5/exercise.5.22.scm
+++ sicp/ch_5/exercise.5.22.scm
@@ -1,12 +1,82 @@
 #!/usr/bin/env csi -s
 
 (use test)
+(require-library machine)
+(import machine)
+(use srfi-1)                            ; append!
 
 ;;; Exercise 5.22
 
-;; *Note Exercise 3-12:: of section *Note 3-3-1::
-;; presented an `append' procedure that appends two lists to form a
-;; new list and an `append!' procedure that splices two lists
-;; together.  Design a register machine to implement each of these
-;; procedures.  Assume that the list-structure memory operations are
-;; available as primitive operations.
+;; *Note Exercise 3-12:: of section *Note 3-3-1:: presented an
+;; `append' procedure that appends two lists to form a new list and an
+;; `append!' procedure that splices two lists together. Design a
+;; register machine to implement each of these procedures. Assume that
+;; the list-structure memory operations are available as primitive
+;; operations.
+
+(define (identity x) x)
+
+(define x '(1 2 3))
+(define y '(4 5 6))
+(test '(1 2 3 4 5 6) (append x y))
+(test '(1 2 3) (identity x))
+(test '(4 5 6) (identity y))
+
+(define x '(1 2 3))
+(define y '(4 5 6))
+(test '(1 2 3 4 5 6) (append! x y))
+(test '(1 2 3 4 5 6) (identity x))
+(test '(4 5 6) (identity y))
+
+;; TODO: such a mental block it isn't funny...
+;;
+;; (define x '(1 2 3))
+;; (define y '(4 5 6))
+;; (test '(1 2 3 4 5 6) (my-append x y))
+;; (test '(1 2 3) (identity x))
+;; (test '(4 5 6) (identity y))
+
+(define (my-append! l1 l2)
+  (if (null? (cdr l1))
+      (set-cdr! l1 l2)
+      (my-append! (cdr l1) l2))
+  l1)
+
+(define (my-append! l1 l2)
+  (define (iterate l)
+    (if (null? (cdr l))
+        (set-cdr! l l2)
+        (iterate (cdr l))))
+  (iterate l1)
+  l1)
+
+(define x '(1 2 3))
+(define y '(4 5 6))
+(test '(1 2 3 4 5 6) (my-append! x y))
+(test '(1 2 3 4 5 6) (identity x))
+(test '(4 5 6) (identity y))
+
+(define machine-append!
+  (make-machine
+   '(tree l1 l2 l t) ; result is in l1
+   (list (list 'cdr cdr) (list 'set-cdr! set-cdr!) (list 'null? null?))
+   '(controller
+       (assign l (reg l1))
+     iter-loop
+       (assign t (op cdr) (reg l))
+       (test (op null?) (reg t))
+       (branch (label done))
+       ;; not done, iterate on cdr
+       (assign l (op cdr) (reg l))
+       (goto (label iter-loop))
+     done
+       (perform (op set-cdr!) (reg l) (reg l2)))))
+
+(define x '(1 2 3))
+(define y '(4 5 6))
+(assert-machine machine-append!
+                (list (list 'l1 x) (list 'l2 y))
+                'l1 '(1 2 3 4 5 6))
+(test '(1 2 3 4 5 6) (identity x))
+(test '(4 5 6) (identity y))
+

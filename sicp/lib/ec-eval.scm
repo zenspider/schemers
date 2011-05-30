2a704e47f8c4ea36785049f678575ac115cbfe7f
Ryan Davis <ryand-ruby@zenspider.com>
2011-05-30 16:11:47 -0700

    soooo much cleaner this way

--- sicp/lib/ec-eval.scm
+++ sicp/lib/ec-eval.scm
@@ -370,33 +370,20 @@
 
        eval-dispatch
 
-       (test (op self-evaluating?) (reg exp))
-       (branch (label ev-self-eval))
-       (test (op variable?) (reg exp))
-       (branch (label ev-variable))
-       (test (op quoted?) (reg exp))
-       (branch (label ev-quoted))
-       (test (op assignment?) (reg exp))
-       (branch (label ev-assignment))
-       (test (op definition?) (reg exp))
-       (branch (label ev-definition))
-       (test (op if?) (reg exp))
-       (branch (label ev-if))
-       (test (op lambda?) (reg exp))
-       (branch (label ev-lambda))
-       (test (op begin?) (reg exp))
-       (branch (label ev-begin))
+       (perform (op debug-exp) (const eval-dispatch) (reg exp))
 
-       (test (op cond?) (reg exp))
-       (branch (label ev-cond))
-
-       (test (op let?) (reg exp))
-       (branch (label ev-let))
-
-       (test (op application?) (reg exp))
-       (branch (label ev-application))
-       (test (op eof?) (reg exp))
-       (branch (label done))
+       (test (op self-evaluating?) (reg exp)) (branch (label ev-self-eval))
+       (test (op variable?)        (reg exp)) (branch (label ev-variable))
+       (test (op quoted?)          (reg exp)) (branch (label ev-quoted))
+       (test (op assignment?)      (reg exp)) (branch (label ev-assignment))
+       (test (op definition?)      (reg exp)) (branch (label ev-definition))
+       (test (op if?)              (reg exp)) (branch (label ev-if))
+       (test (op lambda?)          (reg exp)) (branch (label ev-lambda))
+       (test (op begin?)           (reg exp)) (branch (label ev-begin))
+       (test (op cond?)            (reg exp)) (branch (label ev-cond))
+       (test (op let?)             (reg exp)) (branch (label ev-let))
+       (test (op application?)     (reg exp)) (branch (label ev-application))
+       (test (op eof?)             (reg exp)) (branch (label done))
        (goto (label unknown-expression-type))
 
        ev-self-eval

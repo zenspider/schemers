19ff07909435b6f60ae48403a9ee86ed094b2df9
Ryan Davis <ryand-ruby@zenspider.com>
2011-06-14 14:20:47 -0700

    5.36

--- sicp/ch_5/exercise.5.36.scm
+++ sicp/ch_5/exercise.5.36.scm
@@ -12,3 +12,74 @@
 ;; evaluator in section *Note 5-4-1::.) How does changing the order of
 ;; operand evaluation affect the efficiency of the code that
 ;; constructs the argument list?
+
+(require-library compiler)
+(import compiler)
+
+;; A: right to left, as defined by the reversal of the args when
+;; building up argl for an apply. You can see it in the generated code
+;; below where it starts with calling y, not x.
+
+;; TODO: I have a bug: (statements (compile '(+ (x) (y) (z)) 'val 'return))
+
+(statements (compile '(+ (x) (y)) 'val 'return))
+
+;; ((assign proc (op lookup-variable-value) (const +) (reg env))
+;;   (save continue)
+;;   (save proc)
+;;   (save env)
+;;   (assign proc (op lookup-variable-value) (const y) (reg env))
+;;   (assign argl (const ()))
+;;   (test (op primitive-procedure?) (reg proc))
+;;   (branch (label primitive-branch4))
+;;
+;;   compiled-branch5
+;;
+;;   (assign continue (label after-call6))
+;;   (assign val (op compiled-procedure-entry) (reg proc))
+;;   (goto (reg val))
+;;
+;;   primitive-branch4
+;;
+;;   (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+;;
+;;   after-call6
+;;
+;;   (assign argl (op list) (reg val))
+;;   (restore env)
+;;   (save argl)
+;;   (assign proc (op lookup-variable-value) (const x) (reg env))
+;;   (assign argl (const ()))
+;;   (test (op primitive-procedure?) (reg proc))
+;;   (branch (label primitive-branch1))
+;;
+;;   compiled-branch2
+;;
+;;   (assign continue (label after-call3))
+;;   (assign val (op compiled-procedure-entry) (reg proc))
+;;   (goto (reg val))
+;;
+;;   primitive-branch1
+;;
+;;   (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+;;
+;;   after-call3
+;;
+;;   (restore argl)
+;;   (assign argl (op cons) (reg val) (reg argl))
+;;   (restore proc)
+;;   (restore continue)
+;;   (test (op primitive-procedure?) (reg proc))
+;;   (branch (label primitive-branch7))
+;;
+;;   compiled-branch8
+;;
+;;   (assign val (op compiled-procedure-entry) (reg proc))
+;;   (goto (reg val))
+;;
+;;   primitive-branch7
+;;
+;;   (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+;;   (goto (reg continue))
+;;
+;;   after-call9)

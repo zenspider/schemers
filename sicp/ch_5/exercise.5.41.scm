1a86de618c0744daf30c2800e5007f35bc693b49
Ryan Davis <ryand-ruby@zenspider.com>
2011-07-09 17:45:27 -0700

    5.41

--- sicp/ch_5/exercise.5.41.scm
+++ sicp/ch_5/exercise.5.41.scm
@@ -19,3 +19,17 @@
 ;;
 ;;      (find-variable 'w '((y z) (a b c d e) (x y)))
 ;;      not-found
+
+;; Sucks because there is no way to get obj + index via a findy function
+(define (find-variable var env)
+  (let ((y (list-index (lambda (l) (member var l)) env)))
+    (if y
+        (list y (list-index (lambda (x) (eq? var x)) (list-ref env y)))
+        'not-found)))
+
+(let ((env '((y z)
+             (a b c d e)
+             (x y))))
+  (test '(1 2)     (find-variable 'c env))
+  (test '(2 0)     (find-variable 'x env))
+  (test 'not-found (find-variable 'w env)))

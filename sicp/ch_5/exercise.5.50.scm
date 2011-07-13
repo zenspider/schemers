56e450777d930107f6ea0033cccd46b99baf5bd6
Ryan Davis <ryand-ruby@zenspider.com>
2011-07-13 14:56:59 -0700

    Added tests and WIP code to 5.50... not even CLOSE to finishing

--- sicp/ch_5/exercise.5.50.scm
+++ sicp/ch_5/exercise.5.50.scm
@@ -1,6 +1,8 @@
 #!/usr/bin/env csi -s
 
 (use test)
+(require-library compiler)
+(import compiler)
 
 ;;; Exercise 5.50
 
@@ -11,3 +13,22 @@
 ;; will run very slowly because of the multiple levels of
 ;; interpretation, but getting all the details to work is an
 ;; instructive exercise.
+
+;; TODO
+
+(let ((exp '(cond ((a) 1)
+                  ((b) 2)
+                  (else 3))))
+  (test (cdr exp) (cond-clauses exp))
+  (test '(if (a) 1 (if (b) 2 3)) (cond->if exp)))
+
+(define eval-sexp #f)
+(with-input-from-file "lib/eval.scm"
+  (lambda ()
+    (set! eval-sexp (drop (read) 4))
+    (set-car! eval-sexp 'begin)))
+
+(append-eceval-operations
+ 'pair? pair?)
+
+(assert-compile 42 eval-sexp)

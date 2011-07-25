eb6355176830d9c1ffa0d55c40a8d73b8230746f
Ryan Davis <ryand-ruby@zenspider.com>
2011-07-25 02:21:38 -0700

    TADA!!! 5.50 is done!

--- sicp/ch_5/exercise.5.50.scm
+++ sicp/ch_5/exercise.5.50.scm
@@ -14,21 +14,100 @@
 ;; interpretation, but getting all the details to work is an
 ;; instructive exercise.
 
-;; TODO
-
-(let ((exp '(cond ((a) 1)
-                  ((b) 2)
-                  (else 3))))
-  (test (cdr exp) (cond-clauses exp))
-  (test '(if (a) 1 (if (b) 2 3)) (cond->if exp)))
-
 (define eval-sexp #f)
+
 (with-input-from-file "lib/eval.scm"
   (lambda ()
-    (set! eval-sexp (drop (read) 4))
-    (set-car! eval-sexp 'begin)))
+    (set! eval-sexp (drop (read) 5))))
 
-(append-eceval-operations
- 'pair? pair?)
+(append-primitive-procedures
+ '<           <
+ 'caar        caar
+ 'cadddr      cadddr
+ 'caddr       caddr
+ 'cadr        cadr
+ 'cddr        cddr
+ 'eof-object? eof-object?
+ 'error       error
+ 'list        list
+ 'not         not
+ 'number?     number?
+ 'pair?       pair?
+ 'set-car!    set-car!
+ 'set-cdr!    set-cdr!
+ 'string?     string?
+ 'symbol?     symbol?
+ 'zip         zip)
 
-(assert-compile 42 eval-sexp)
+(define (test-sexp exp)
+  (append
+   '(begin)
+   (append
+    eval-sexp
+    `((define my-env (setup-environment))
+      (eval ,exp my-env)))))
+
+(test-group "holy crap I'm done!"
+  (assert-compile  42 (test-sexp '42))
+  (assert-compile  24 (test-sexp '24))
+  (assert-compile  #t (test-sexp 'true))
+  (assert-compile  #f (test-sexp 'false))
+  (assert-compile   3 (test-sexp '(+ 1 2)))
+  (assert-compile 120 (test-sexp '(* 1 2 3 4 5)))
+  (assert-compile "good" (test-sexp '(begin
+                                       (define (append x y)
+                                         (if (null? x)
+                                             y
+                                             (cons (car x) (append (cdr x) y))))
+                                       (let ((l (append '(a b c) '(d e f))))
+                                         (cond
+                                          ((eq? (length l) 5) "bad")
+                                          ((eq? (length l) 6) "good")
+                                          (else 'else))))))
+
+  ;; HACK; prolly should be '*undefined* or somesuch
+  (assert-compile #f (test-sexp '(begin
+                                   (define (append x y)
+                                     (if (null? x)
+                                         y
+                                         (cons (car x) (append (cdr x) y))))
+                                   (let ((l (append '(a b c) '(d))))
+                                     (cond
+                                      ((eq? (length l) 5) "bad")
+                                      ((eq? (length l) 6) "good"))))))
+
+  (assert-compile "else" (test-sexp '(begin
+                                       (define (append x y)
+                                         (if (null? x)
+                                             y
+                                             (cons (car x) (append (cdr x) y))))
+
+                                       (let ((l (append '(a b c) '(d))))
+                                         (cond
+                                          ((eq? (length l) 5) "bad")
+                                          ((eq? (length l) 6) "good")
+                                          (else "else"))))))
+
+  (assert-compile 120 (test-sexp '(begin
+                                    (define (factorial-r n)
+                                      (if (= n 1)
+                                          1
+                                          (* (factorial-r (- n 1)) n)))
+                                    (factorial-r 5))))
+
+  (assert-compile 120 (test-sexp '(begin
+                                    (define (factorial-i n)
+                                      (define (iter product counter)
+                                        (if (> counter n)
+                                            product
+                                            (iter (* counter product)
+                                                  (+ counter 1))))
+                                      (iter 1 1))
+                                    (factorial-i 5))))
+
+  (assert-compile 55 (test-sexp '(begin
+                                   (define (fib n)
+                                     (if (< n 2)
+                                         n
+                                         (+ (fib (- n 1)) (fib (- n 2)))))
+                                   (fib 10)))))

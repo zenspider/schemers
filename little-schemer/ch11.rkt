a42f577b374dd78941898d513bdd0db6c58f2d86
Ryan Davis <ryand-ruby@zenspider.com>
2014-12-15 16:05:40 -0800

    Cleaned up ch11 tests

--- little-schemer/ch11.rkt
+++ little-schemer/ch11.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 
-(require rackunit)
 (require "lib/shared.rkt")
+
+(module+ test
+  (require rackunit))
 
 (define two-in-a-row-b?
   (lambda (preceding lat)
@@ -15,12 +17,13 @@
           [else
            (two-in-a-row-b? (car lat) (cdr lat))])))
 
-(test-case "two-in-a-row?"
-  (check-false (two-in-a-row? '(a b c d)))
-  (check-false (two-in-a-row? '(a b a d)))
-  (check-true (two-in-a-row? '(a b b d)))
-  (check-true (two-in-a-row? '(a a c d)))
-  (check-true (two-in-a-row? '(a b c c))))
+(module+ test
+  (test-case "two-in-a-row?"
+    (check-false (two-in-a-row? '(a b c d)))
+    (check-false (two-in-a-row? '(a b a d)))
+    (check-true (two-in-a-row? '(a b b d)))
+    (check-true (two-in-a-row? '(a a c d)))
+    (check-true (two-in-a-row? '(a b c c)))))
 
 (define sum-of-prefixes-b
   (lambda (sum tup)
@@ -32,11 +35,12 @@
   (lambda (tup)
     (sum-of-prefixes-b 0 tup)))
 
-(test-case "sum-of-prefixes"
-  (check-equal? (sum-of-prefixes '(1 1 1 1 1))
-                '(1 2 3 4 5))
-  (check-equal? (sum-of-prefixes '(2 1 9 17 0))
-                '(2 3 12 29 29)))
+(module+ test
+  (test-case "sum-of-prefixes"
+    (check-equal? (sum-of-prefixes '(1 1 1 1 1))
+                  '(1 2 3 4 5))
+    (check-equal? (sum-of-prefixes '(2 1 9 17 0))
+                  '(2 3 12 29 29))))
 
 (define pick
   (lambda (n lat)
@@ -56,8 +60,9 @@
   (lambda (tup)
     (scramble-b tup '())))
 
-(test-case "scramble"
-  (check-equal? (scramble '(1 1 1 3 4 2 1 1 9 2))
-                '(1 1 1 1 1 4 1 1 1 9))
-  (check-equal? (scramble '(1 2 3 4 5 6 7 8 9))
-                '(1 1 1 1 1 1 1 1 1)))
+(module+ test
+  (test-case "scramble"
+    (check-equal? (scramble '(1 1 1 3 4 2 1 1 9 2))
+                  '(1 1 1 1 1 4 1 1 1 9))
+    (check-equal? (scramble '(1 2 3 4 5 6 7 8 9))
+                  '(1 1 1 1 1 1 1 1 1))))

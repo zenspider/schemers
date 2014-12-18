29ad6269755bb39c7a1d109ace39cbad081921df
Ryan Davis <ryand-ruby@zenspider.com>
2014-12-17 18:33:20 -0800

    moved up

--- smalltalk/lexer.rkt
+++ smalltalk/lexer.rkt
@@ -6,6 +6,43 @@
 (require parser-tools/lex)                   ; lexer-src-pos
 (require (prefix-in : parser-tools/lex-sre)) ; regexps
 
+;; From the Blue Book. Consider this a TODO:
+;;
+;; digit       = [0-9]
+;; digits      = (digit)+
+;; number      = (digits "r")? ("-")? digits ("." digits)? ("e" ("-")? digits)?
+;; letter      = [A-Za-z]
+;; special     = [+/\*~<>=@%|&?!]
+;; char        = digit | lettter | special | [\[\]{}()_^,;$!#:]
+;; ident       = letter (letter|digit)*
+;; symbol      = ident | binary_sel | (keyword)+
+;; sym_const   = "#" symbol
+;; char_const  = "$" (char | "'" | "\"")
+;; string      = "'" (char | "'" "'" | "\"")* "'"
+;; comment     = "\"" (char | "\"" "\"" | "'")* "\""
+;; array       = "(" (number | symbol | string | char_const | array)* ")"
+;; array_const = "#" array
+;; literal     = number | sym_const | char_const | string | array_const
+;; var_name    = ident
+;; unary_sel   = ident
+;; binary_sel  = "-" | special (special)?
+;; keyword     = ident ":"
+;; primary     = var_name | literal | block | "(" expression ")"
+;; unary_obj   = primary | unary_exp
+;; binary_obj  = unary_obj | binary_exp
+;; unary_exp   = unary_obj unary_sel
+;; binary_exp  = binary_obj binary_sel unary_obj
+;; keyword_exp = binary_obj (keyword binary_obj)+
+;; message_exp = unary_exp | binary_exp | keyword_exp
+;; cascade_exp = message_exp (";" unary_sel | binary_sel unary_obj | (keyword binary_obj)+ )+
+;; expr        = (var_name "_")? (primary | message_exp | cascade_exp)
+;; statements  = ( | "^" expr | (expr "." statements) ) # FIX
+
+;; block = "[" ((":" var_name)+ "|")? (statements)? "]"
+;; temporaries = "|" (var_name)* "|"
+;; message_pat = unary_sel | binary_sel var_name | (keyword var_name)+
+;; method      = message_pat (temporaries)? (statements)?
+
 (define-lex-abbrev ws (:* whitespace))
 (define-lex-abbrev any any-string)
 (define-lex-abbrev key-chars1 (char-set "/<>%&?,+=@-\\*~"))
@@ -19,41 +56,6 @@
    [(eof)                                      (return-without-pos eof)]
    [(:or #\tab #\space #\newline #\page)       (skip)]
    [(:: #\" (complement (:: any #\" any)) #\") (skip)]
-
-   ;; digit       = [0-9]
-   ;; digits      = (digit)+
-   ;; number      = (digits "r")? ("-")? digits ("." digits)? ("e" ("-")? digits)?
-   ;; letter      = [A-Za-z]
-   ;; special     = [+/\*~<>=@%|&?!]
-   ;; char        = digit | lettter | special | [\[\]{}()_^,;$!#:]
-   ;; ident       = letter (letter|digit)*
-   ;; symbol      = ident | binary_sel | (keyword)+
-   ;; sym_const   = "#" symbol
-   ;; char_const  = "$" (char | "'" | "\"")
-   ;; string      = "'" (char | "'" "'" | "\"")* "'"
-   ;; comment     = "\"" (char | "\"" "\"" | "'")* "\""
-   ;; array       = "(" (number | symbol | string | char_const | array)* ")"
-   ;; array_const = "#" array
-   ;; literal     = number | sym_const | char_const | string | array_const
-   ;; var_name    = identifier
-   ;; unary_sel   = identifier
-   ;; binary_sel  = "-" | special (special)?
-   ;; keyword     = identifier ":"
-   ;; primary     = var_name | literal | block | "(" expression ")"
-   ;; unary_obj   = primary | unary_exp
-   ;; binary_obj  = unary_obj | binary_exp
-   ;; unary_exp   = unary_obj unary_sel
-   ;; binary_exp  = binary_obj binary_sel unary_obj
-   ;; keyword_exp = binary_obj (keyword binary_obj)+
-   ;; message_exp = unary_exp | binary_exp | keyword_exp
-   ;; cascade_exp = message_exp (";" unary_sel | binary_sel unary_obj | (keyword binary_obj)+ )+
-   ;; expr        = (var_name "_")? (primary | message_exp | cascade_exp)
-   ;; statements  = ( | "^" expr | (expr "." statements) ) # FIX
-
-   ;; block = "[" ((":" var_name)+ "|")? (statements)? "]"
-   ;; temporaries = "|" (var_name)* "|"
-   ;; message_pat = unary_sel | binary_sel var_name | (keyword var_name)+
-   ;; method      = message_pat (temporaries)? (statements)?
 
    [(:: key-chars1 (:? key-chars2))                   (token 'BIN  lexeme)]
    [(:: alphabetic (:+ (:or alphabetic numeric)) ":") (token 'KEY  lexeme)]

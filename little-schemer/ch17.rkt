8b7960d1aa321df58a09d46c4cdfb0ec90aa1801
Ryan Davis <ryand-ruby@zenspider.com>
2015-03-24 18:34:14 -0700

    mostly formatting.

--- little-schemer/ch17.rkt
+++ little-schemer/ch17.rkt
@@ -11,10 +11,10 @@
 
 (define find                            ; from pg 113
   (lambda (n Ns Rs)
-    (letrec ((A (lambda (ns rs)
+    (letrec ([A (lambda (ns rs)
                   (cond [(null? ns) #f]
                         [(= (car ns) n) (car rs)]
-                        [else (A (cdr ns) (cdr rs))]))))
+                        [else (A (cdr ns) (cdr rs))]))])
       (A Ns Rs))))
 
 (define deep1
@@ -25,17 +25,17 @@
               '()))))
 
 (define deepM1
-  (let ((Rs '())
-        (Ns '()))
-    (letrec ((D (lambda (m)
+  (let ([Rs '()]
+        [Ns '()])
+    (letrec ([D (lambda (m)
                   (if (zero? m)
                       'pizza
                       (cons (D (sub1 m))
-                            '())))))
+                            '())))])
       (lambda (n)
-        (let ((exists (find n Ns Rs)))
+        (let ([exists (find n Ns Rs)])
           (when (atom? exists)
-            (let ((result (D n)))
+            (let ([result (D n)])
               (set! Rs (cons result Rs))
               (set! Ns (cons n Ns))
               result)))))))
@@ -45,17 +45,17 @@
                 '(((pizza)))))
 
 (define deepM2                           ; call deepM2 instead of D
-  (let ((Rs '())
-        (Ns '()))
-    (letrec ((D (lambda (m)
+  (let ([Rs '()]
+        [Ns '()])
+    (letrec ([D (lambda (m)
                   (if (zero? m)
                       'pizza
                       (cons (deepM2 (sub1 m))
-                            '())))))
+                            '())))])
       (lambda (n)
-        (let ((exists (find n Ns Rs)))
+        (let ([exists (find n Ns Rs)])
           (when (atom? exists)
-            (let ((result (D n)))
+            (let ([result (D n)])
               (set! Rs (cons result Rs))
               (set! Ns (cons n Ns))
               result)))))))
@@ -65,17 +65,17 @@
                 '(((pizza)))))
 
 (define deepM3                           ; merge letrec into let
-  (let ((Rs '())
-        (Ns '())
-        (D (lambda (m)
+  (let ([Rs '()]
+        [Ns '()]
+        [D (lambda (m)
              (if (zero? m)
                  'pizza
                  (cons (deepM3 (sub1 m))
-                       '())))))
+                       '())))])
     (lambda (n)
-      (let ((exists (find n Ns Rs)))
+      (let ([exists (find n Ns Rs)])
         (when (atom? exists)
-          (let ((result (D n)))
+          (let ([result (D n)])
             (set! Rs (cons result Rs))
             (set! Ns (cons n Ns))
             result))))))
@@ -85,15 +85,15 @@
                 '(((pizza)))))
 
 (define deepM4                           ; unfactor D
-  (let ((Rs '())
-        (Ns '()))
+  (let ([Rs '()]
+        [Ns '()])
     (lambda (n)
-      (let ((exists (find n Ns Rs)))
+      (let ([exists (find n Ns Rs)])
         (when (atom? exists)
-          (let ((result (if (zero? n)
+          (let ([result (if (zero? n)
                             'pizza
                             (cons (deepM4 (sub1 n))
-                                  '()))))
+                                  '()))])
             (set! Rs (cons result Rs))
             (set! Ns (cons n Ns))
             result))))))
@@ -108,7 +108,7 @@
 (define set-counter #f)
 
 (define consC
-  (let ((N 0))
+  (let ([N 0])
     (set! counter (lambda () N))
     (set! set-counter (lambda (n) (set! N n)))
     (lambda (x y)
@@ -144,12 +144,12 @@
 
 (define supercounter
   (lambda (f)
-    (letrec ((S (lambda (n)
+    (letrec ([S (lambda (n)
                   (if (zero? n)
                       (f n)
                       (let ()
                         (f n)
-                        (S (sub1 n)))))))
+                        (S (sub1 n)))))])
       (S 1000))))
 
 (module+ test
@@ -159,15 +159,15 @@
                 500500))
 
 (define deepM5
-  (let ((Rs '())
-        (Ns '()))
+  (let ([Rs '()]
+        [Ns '()])
     (lambda (n)
-      (let ((exists (find n Ns Rs)))
+      (let ([exists (find n Ns Rs)])
         (when (atom? exists)
-          (let ((result (if (zero? n)
+          (let ([result (if (zero? n)
                             'pizza
                             (consC (deepM5 (sub1 n))
-                                   '()))))
+                                   '()))])
             (set! Rs (cons result Rs))
             (set! Ns (cons n Ns))
             result))))))
@@ -190,20 +190,20 @@
 
 (define rember1*C
   (lambda (a l)
-    (letrec ((R (lambda (l oh)
+    (letrec ([R (lambda (l oh)
                   (cond
-                   [(null? l) (oh 'no)]
-                   [(atom? (car l))
-                    (if (eq? (car l) a)
-                        (cdr l)
-                        (consC (car l)
-                               (R (cdr l) oh)))]
-                   [else
-                    (let ((new-car (let/cc oh (R (car l) oh))))
-                      (when (atom? new-car)
-                        (consC (car l)
-                               (R (cdr l) oh))))]))))
-      (let ((new-l (let/cc oh (R l oh))))
+                    [(null? l) (oh 'no)]
+                    [(atom? (car l))
+                     (if (eq? (car l) a)
+                         (cdr l)
+                         (consC (car l)
+                                (R (cdr l) oh)))]
+                    [else
+                     (let ([new-car (let/cc oh (R (car l) oh))])
+                       (when (atom? new-car)
+                         (consC (car l)
+                                (R (cdr l) oh))))]))])
+      (let ([new-l (let/cc oh (R l oh))])
         (if (atom? new-l)
             l
             new-l)))))

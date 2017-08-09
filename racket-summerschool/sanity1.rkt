1665c077701f3d7c7c4804620a60b34c71ec93f8
Ryan Davis <ryand-ruby@zenspider.com>
2017-08-08 19:43:15 -0700

    Revamped tests to parameterize default-language and refactor tests for reusability.

--- racket-summerschool/sanity1.rkt
+++ racket-summerschool/sanity1.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require redex/reduction-semantics)
+(check-redundancy #t)
 
 ;;; PureLambda - nothing extra at all
 
@@ -31,19 +32,21 @@
          apply)))
 
 (module+ test
-  (letrec ([x️ (lambda (i e) (test-->  PureLambda-> i e))]
-           [X️ (lambda (i e) (test-->> PureLambda-> i e))])
+  (define e01 (term ((λ (x) x) x)))
+  (define e02 (term ((λ (x) x)
+                     (λ (x) x))))
+  (define e03 (term ((λ (x) (λ (y) x))
+                     ((λ (x) x) z))))
 
-    (define e01 (term ((λ (x) x) x)))
-    (define e02 (term ((λ (x) x)
-                       (λ (x) x))))
-    (define e03 (term ((λ (x) (λ (y) x))
-                       ((λ (x) x) z))))
+  (parameterize ([default-language PureLambda])
+   (letrec ([x (lambda (i e) (test-->  PureLambda-> i e))]
+            [X (lambda (i e) (test-->> PureLambda-> i e))])
 
-    (x️ e01 (term x))
-    (x️ e02 (term (λ (x) x)))
-    (X️ e03 (term (λ (y) z)))))
+     (x e01 (term x))
+     (x e02 (term (λ (x) x)))
+     (X e03 (term (λ (y) z))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Lambda - extend PureLambda with simple values and operations
 
 (define-extended-language Lambda PureLambda
@@ -107,34 +110,37 @@
   )
 
 (module+ test
-  (letrec ([x (lambda (i e) (test-->  Lambda-> i e))]
-           [X (lambda (i e) (test-->> Lambda-> i e))])
+  (define e04 (term (+ 1 2)))
+  (define e05 (term (+ 1 (+ 2 3))))
+  (define e06 (term (- 5 2)))
+  (define e07 (term (- 3 (- 2 1))))
+  (define e08 (term (++ "a" "b")))
+  (define e09 (term (++ "a" (++ "b" "c"))))
+  (define e10 (term (nand #t #t)))
+  (define e11 (term (nand #f #f)))
+  (define e12 (term (if #t 1 2)))
+  (define e13 (term (if #f 1 2)))
 
-    (define e04 (term (+ 1 2)))
-    (define e05 (term (+ 1 (+ 2 3))))
-    (define e06 (term (- 5 2)))
-    (define e07 (term (- 3 (- 2 1))))
-    (define e08 (term (++ "a" "b")))
-    (define e09 (term (++ "a" (++ "b" "c"))))
-    (define e10 (term (nand #t #t)))
-    (define e11 (term (nand #f #f)))
-    (define e12 (term (if #t 1 2)))
-    (define e13 (term (if #f 1 2)))
+  (parameterize ([default-language Lambda])
+   (letrec ([x (lambda (i e) (test-->  Lambda-> i e))]
+            [X (lambda (i e) (test-->> Lambda-> i e))])
 
-    (x e04 (term 3))
-    (X e05 (term 6))
-    (x e06 (term 3))
-    (X e07 (term 2))
-    (x e08 (term "ab"))
-    (x (term (++ "a" (+ 1 2))) (term (++ "a" 3))) ; TODO: failure
-    (X e09 (term "abc"))
-    (x e10 (term #f))
-    (x e11 (term #t))
-    (x e12 (term 1))
-    (x e13 (term 2))
-    ))
+     (x e01 (term x))                   ; previous tests
+     (x e02 (term (λ (x) x)))
+     (X e03 (term (λ (y) z)))
 
-(default-language Lambda) ; Defines alpha-equivalence (eg test-equal)
+     (x e04 (term 3))
+     (X e05 (term 6))
+     (x e06 (term 3))
+     (X e07 (term 2))
+     (x e08 (term "ab"))
+     (x (term (++ "a" (+ 1 2))) (term (++ "a" 3))) ; TODO: failure
+     (X e09 (term "abc"))
+     (x e10 (term #f))
+     (x e11 (term #t))
+     (x e12 (term 1))
+     (x e13 (term 2))
+     )))
 
 ;; (define t (term (if #t 1 2)))
 ;; (redex-match Lambda e t)
